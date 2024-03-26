286b4d5fe1e8dc9c5b18967d56df3e7e052e7c92
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-03-26 15:09:23 +0900

    up

--- sandbox/prototypeProjectTes/240326_1436.py
+++ sandbox/prototypeProjectTes/240326_1436.py
@@ -1,16 +1,41 @@
 from pyrubicon.objc.api import ObjCInstance, ObjCClass, ObjCProtocol
 from pyrubicon.objc.api import objc_property, objc_method
+from pyrubicon.objc.api import NSObject
 from pyrubicon.objc.api import Block
-from pyrubicon.objc.runtime import SEL, send_super
+from pyrubicon.objc.runtime import SEL#, send_super
 
 from dispatchSync import dispatch_sync
 
 import pdbr
 
+
+def present_ViewController(viewController_instance):
+  vc = viewController_instance
+  app = ObjCClass('UIApplication').sharedApplication
+  window = app.keyWindow if app.keyWindow else app.windows[0]
+  root_vc = window.rootViewController
+
+  while root_vc.presentedViewController:
+    root_vc = root_vc.presentedViewController
+
+  @Block
+  def processing() -> None:
+    nv = WrapNavigationController.alloc().initWithRootViewController_(vc)
+    nv.delegate = nv
+    nv.setModalPresentationStyle_(1)
+
+    root_vc.presentViewController_animated_completion_(nv, True, None)
+
+  dispatch_sync(processing)
+
+
 UINavigationController = ObjCClass('UINavigationController')
 UINavigationControllerDelegate = ObjCProtocol('UINavigationControllerDelegate')
 UINavigationBarAppearance = ObjCClass('UINavigationBarAppearance')
 UIBarButtonItem = ObjCClass('UIBarButtonItem')
+
+UIViewController = ObjCClass('UIViewController')
+UIColor = ObjCClass('UIColor')
 
 
 class WrapNavigationController(UINavigationController,
@@ -45,16 +70,13 @@ class WrapNavigationController(UINavigationController,
     navigationItem.rightBarButtonItem = done_btn
 
 
-UIViewController = ObjCClass('UIViewController')
-UIColor = ObjCClass('UIColor')
-
-
 class TopViewController(UIViewController, auto_rename=True):
   generator = objc_property()
 
   @objc_method
   def viewDidLoad(self):
-    send_super(__class__, self, 'viewDidLoad')
+    #send_super(__class__, self, 'viewDidLoad')
+    pass
 
     #self.generator = AudioEngeneWaveGenerator.alloc().initGenerator()
     #self.generator.start()
@@ -65,27 +87,25 @@ class TopViewController(UIViewController, auto_rename=True):
     pass
 
 
-def present_ViewController(viewController_instance):
-  vc = viewController_instance
-  app = ObjCClass('UIApplication').sharedApplication
-  window = app.keyWindow if app.keyWindow else app.windows[0]
-  root_vc = window.rootViewController
+# --- AVAudioEngine
 
-  while root_vc.presentedViewController:
-    root_vc = root_vc.presentedViewController
+AVAudioEngine = ObjCClass('AVAudioEngine')
 
-  @Block
-  def processing() -> None:
-    nv = WrapNavigationController.alloc().initWithRootViewController_(vc)
-    nv.delegate = nv
-    nv.setModalPresentationStyle_(1)
 
-    root_vc.presentViewController_animated_completion_(nv, True, None)
+class AudioEngeneWaveGenerator(NSObject, auto_rename=True):
+  audioEngine: AVAudioEngine = objc_property()
+  #audioEngine = AVAudioEngine.new()
+  
 
-  dispatch_sync(processing)
+  @objc_method
+  def initAudioEngene(self):
+    self.audioEngine = AVAudioEngine.new()
+    pdbr.state(self.audioEngine)
+    return self
 
 
 if __name__ == "__main__":
   _vc = TopViewController.new()
+  _generator = AudioEngeneWaveGenerator.alloc().initAudioEngene()
   present_ViewController(_vc)
 
