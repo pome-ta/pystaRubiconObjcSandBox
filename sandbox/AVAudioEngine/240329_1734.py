dd6db24fe2e8b4df4a931a172dc27c6b83b81507
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-03-29 20:57:02 +0900

    up

---
+++ sandbox/AVAudioEngine/240329_1734.py
@@ -0,0 +1,52 @@
+from math import sin, pi
+import ctypes
+from pyrubicon.objc.api import ObjCClass, Block
+
+import pdbr
+
+AVAudioEngine = ObjCClass('AVAudioEngine')
+AVAudioPlayerNode = ObjCClass('AVAudioPlayerNode')
+AVAudioPCMBuffer = ObjCClass('AVAudioPCMBuffer')
+
+audioEngine = AVAudioEngine.new()
+player = AVAudioPlayerNode.new()
+
+
+def playSineWave():
+  audioFormat = player.outputFormatForBus_(0)
+
+  sampleRate = audioFormat.sampleRate
+  length = 3.0 * sampleRate
+
+  buffer = AVAudioPCMBuffer.alloc().initWithPCMFormat_frameCapacity_(
+    audioFormat, ctypes.c_uint32(int(length)))
+
+  buffer.frameLength = ctypes.c_uint32(int(length))
+
+  channels = int(audioFormat.channelCount)
+
+  for ch in range(channels):
+    samples = buffer.floatChannelData[ch]
+    for n in range(buffer.frameLength):
+      samples[n] = sin(2.0 * pi * 440.0 * float(n) / sampleRate)
+
+  audioEngine.attachNode_(player)
+  mixer = audioEngine.mainMixerNode
+  audioEngine.connect_to_format_(player, mixer, audioFormat)
+
+  @Block
+  def completionHandler() -> ctypes.c_void_p:
+    print('Play completed')
+
+  player.scheduleBuffer_completionHandler_(buffer, completionHandler)
+
+  try:
+    audioEngine.startAndReturnError_(None)
+    player.play()
+  except:
+    print('error')
+
+
+if __name__ == "__main__":
+  playSineWave()
+
