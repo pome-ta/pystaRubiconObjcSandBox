8abf4351dc7f84e250a73ea7fcf31065ae82c169
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-03-30 15:36:13 +0900

    up

--- sandbox/AVAudioEngine/pdbr.py
+++ sandbox/AVAudioEngine/pdbr.py
@@ -2,7 +2,7 @@ import ctypes
 import json
 from pprint import pprint
 
-from pyrubicon.objc.api import ObjCInstance,  NSObject
+from pyrubicon.objc.api import ObjCInstance, NSObject
 from pyrubicon.objc.runtime import libobjc
 
 __all__ = [
@@ -20,7 +20,7 @@ NSObject_instance_methods = [
 ]
 
 
-def _get_className_methods(rubicon_object, is_revers:bool=True):
+def _get_className_methods(rubicon_object, is_revers: bool = True):
   objct_class = libobjc.object_getClass(rubicon_object)
   py_className_methods = {}
 
@@ -47,13 +47,12 @@ def _get_className_methods(rubicon_object, is_revers:bool=True):
       py_className_methods[str(NSObject)] = NSObject_instance_methods
       break
 
-
   _items = py_className_methods.items()
   _list_items = list(reversed(_items) if is_revers else _items)
   return dict(_list_items)
 
 
-def state(rubicon_obj, indent:int=2, is_reverse:bool=True):
+def state(rubicon_obj, indent: int = 2, is_reverse: bool = True):
   _dic = _get_className_methods(rubicon_obj, is_reverse)
   data = json.dumps(_dic, indent=indent)
   print(data)
