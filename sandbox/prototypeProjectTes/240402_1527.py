dd84c574da219adae452149e37437743e67e9c1c
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-04-02 16:00:04 +0900

    ui

---
+++ sandbox/prototypeProjectTes/240402_1527.py
@@ -0,0 +1,119 @@
+import ctypes
+
+from pyrubicon.objc.api import ObjCInstance, ObjCClass, ObjCProtocol
+from pyrubicon.objc.api import objc_property, objc_method
+from pyrubicon.objc.api import NSObject
+from pyrubicon.objc.api import Block
+from pyrubicon.objc.runtime import SEL, send_super
+
+from dispatchSync import dispatch_sync
+
+import pdbr
+
+
+def present_ViewController(viewController_instance):
+  vc = viewController_instance
+  app = ObjCClass('UIApplication').sharedApplication
+  window = app.keyWindow if app.keyWindow else app.windows[0]
+  root_vc = window.rootViewController
+
+  while root_vc.presentedViewController:
+    root_vc = root_vc.presentedViewController
+
+  @Block
+  def processing() -> None:
+    nv = WrapNavigationController.alloc().initWithRootViewController_(vc)
+    nv.delegate = nv
+    nv.setModalPresentationStyle_(1)
+
+    root_vc.presentViewController_animated_completion_(nv, True, None)
+
+  dispatch_sync(processing)
+
+
+UINavigationController = ObjCClass('UINavigationController')
+UINavigationControllerDelegate = ObjCProtocol('UINavigationControllerDelegate')
+UINavigationBarAppearance = ObjCClass('UINavigationBarAppearance')
+UIBarButtonItem = ObjCClass('UIBarButtonItem')
+
+
+class WrapNavigationController(UINavigationController,
+                               protocols=[UINavigationControllerDelegate],
+                               auto_rename=True):
+
+  @objc_method
+  def doneButtonTapped_(self, sender):
+    visibleViewController = self.visibleViewController
+    visibleViewController.dismissViewControllerAnimated_completion_(True, None)
+
+  @objc_method
+  def navigationController_willShowViewController_animated_(
+      self, navigationController, viewController, animated):
+    appearance = UINavigationBarAppearance.alloc()
+    appearance.configureWithDefaultBackground()
+
+    navigationBar = navigationController.navigationBar
+    navigationBar.standardAppearance = appearance
+    navigationBar.scrollEdgeAppearance = appearance
+    navigationBar.compactAppearance = appearance
+    navigationBar.compactScrollEdgeAppearance = appearance
+
+    viewController.setEdgesForExtendedLayout_(0)
+
+    done_btn = UIBarButtonItem.alloc(
+    ).initWithBarButtonSystemItem_target_action_(0, navigationController,
+                                                 SEL('doneButtonTapped:'))
+    visibleViewController = navigationController.visibleViewController
+
+    navigationItem = visibleViewController.navigationItem
+    navigationItem.rightBarButtonItem = done_btn
+
+
+# --- ViewController
+UIViewController = ObjCClass('UIViewController')
+UIColor = ObjCClass('UIColor')
+
+UIButtonConfiguration = ObjCClass('UIButtonConfiguration')
+UIButton = ObjCClass('UIButton')
+
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+
+class FirstViewController(UIViewController, auto_rename=True):
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+
+    self.navigationItem.title = 'FirstViewController'
+    self.view.backgroundColor = UIColor.systemBlueColor()
+
+    config = UIButtonConfiguration.tintedButtonConfiguration()
+    config.title = 'Tap'
+    config.baseBackgroundColor = UIColor.systemPinkColor()
+    config.baseForegroundColor = UIColor.systemGreenColor()
+
+    btn = UIButton.new()
+    btn.configuration = config
+
+    self.view.addSubview_(btn)
+
+    btn.translatesAutoresizingMaskIntoConstraints = False
+    NSLayoutConstraint.activateConstraints_([
+      btn.centerXAnchor.constraintEqualToAnchor_(self.view.centerXAnchor),
+      btn.centerYAnchor.constraintEqualToAnchor_(self.view.centerYAnchor),
+      btn.widthAnchor.constraintEqualToAnchor_multiplier_(
+        self.view.widthAnchor, 0.4),
+      btn.heightAnchor.constraintEqualToAnchor_multiplier_(
+        self.view.heightAnchor, 0.1),
+    ])
+
+  @objc_method
+  def viewWillDisappear_(self, animated: bool):
+    send_super(__class__, self, 'viewWillDisappear:')
+
+
+if __name__ == "__main__":
+  _fvc = FirstViewController.new()
+  present_ViewController(_fvc)
+
