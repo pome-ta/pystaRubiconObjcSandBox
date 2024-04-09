82925691a3548ba2c15434c474e2e46e93dbcf91
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-04-09 17:20:41 +0900

    up

--- sandbox/controllerLifeCycle/240408_1126.py
+++ sandbox/controllerLifeCycle/240408_1126.py
@@ -1,11 +1,18 @@
-from pyrubicon.objc.api import ObjCClass, objc_method
-from pyrubicon.objc.runtime import send_super
+from pyrubicon.objc.api import ObjCClass, ObjCProtocol, objc_method
+from pyrubicon.objc.runtime import SEL, send_super
 
 from mainThread import onMainThread
 from objc_util import on_main_thread
 import pdbr
 
 ObjCClass.auto_rename = True
+ObjCProtocol.auto_rename = True
+
+# --- UINavigationController
+UINavigationController = ObjCClass('UINavigationController')
+UINavigationControllerDelegate = ObjCProtocol('UINavigationControllerDelegate')
+UINavigationBarAppearance = ObjCClass('UINavigationBarAppearance')
+UIBarButtonItem = ObjCClass('UIBarButtonItem')
 
 # --- UIViewController
 UIViewController = ObjCClass('UIViewController')
@@ -13,12 +20,74 @@ UIColor = ObjCClass('UIColor')
 pageSheet = 1  # xxx: あとでちゃんと定義する
 
 
+# --- NavigationController
+class RootNavigationController(UINavigationController,
+                               protocols=[UINavigationControllerDelegate]):
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    print('--- viewDidLoad\t -> NavigationController')
+    self.delegate = self
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    # xxx: 引数不要?
+    send_super(__class__, self, 'viewDidAppear:')
+    print('--- viewDidAppear:\t -> NavigationController')
+
+  @objc_method
+  def viewWillDisappear_(self, animated: bool):
+    send_super(__class__, self, 'viewWillDisappear:')
+    print('--- viewWillDisappear:\t -> NavigationController')
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__, self, 'viewDidDisappear:')
+    print('--- viewDidDisappear:\t -> NavigationController')
+
+  @objc_method
+  def navigationController_willShowViewController_animated_(
+      self, navigationController, viewController, animated):
+    print('--- :willShowViewController:animated:\t -> NavigationController')
+    appearance = UINavigationBarAppearance.new()
+    appearance.configureWithDefaultBackground()
+
+    navigationBar = navigationController.navigationBar
+    navigationBar.standardAppearance = appearance
+    navigationBar.scrollEdgeAppearance = appearance
+    navigationBar.compactAppearance = appearance
+    navigationBar.compactScrollEdgeAppearance = appearance
+
+    viewController.setEdgesForExtendedLayout_(0)
+
+
+# --- ViewController
 class FirstViewController(UIViewController):
 
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
+    print('--- ViewController-> viewDidLoad')
     self.view.backgroundColor = UIColor.systemBlueColor()
+    self.navigationItem.title = str(__class__)
+    #pdbr.state(self.navigationItem)
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    # xxx: 引数不要？
+    send_super(__class__, self, 'viewDidAppear:')
+    print('--- ViewController-> viewDidAppear:')
+
+  @objc_method
+  def viewWillDisappear_(self, animated: bool):
+    send_super(__class__, self, 'viewWillDisappear:')
+    print('--- ViewController-> viewWillDisappear:')
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__, self, 'viewDidDisappear:')
+    print('--- ViewController-> viewDidDisappear:')
 
 
 @onMainThread
@@ -30,13 +99,15 @@ def present_viewController(myVC: UIViewController):
   while _presentedVC := rootVC.presentedViewController:
     rootVC = _presentedVC
 
-  presentVC = myVC
+  myNC = RootNavigationController.alloc().initWithRootViewController_(myVC)
+
+  presentVC = myNC
   presentVC.setModalPresentationStyle_(pageSheet)
 
   rootVC.presentViewController_animated_completion_(presentVC, True, None)
 
 
 if __name__ == "__main__":
-  vc  = FirstViewController.new()
+  vc = FirstViewController.new()
   present_viewController(vc)
 
