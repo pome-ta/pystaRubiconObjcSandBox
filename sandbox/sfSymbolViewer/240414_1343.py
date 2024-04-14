ec48d40a80926c8ae53bdad2dcad694313b23ae6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-04-14 17:43:33 +0900

    up

--- sandbox/sfSymbolViewer/240414_1343.py
+++ sandbox/sfSymbolViewer/240414_1343.py
@@ -19,23 +19,33 @@ UIButtonConfiguration = ObjCClass('UIButtonConfiguration')
 UIButton = ObjCClass('UIButton')
 
 NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
-# ref: [UIRectEdge | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uirectedge?language=objc)
-'''
-UIRectEdgeNone = 0
-'''
-edgeNone = 0
 
-# ref: [UIControlEvents | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uicontrolevents?language=objc)
-'''
-UIControlEventTouchUpInside = 1 <<  6
-'''
-touchUpInside = 1 << 6
 
-# ref: [UIBarButtonSystemItem | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uibarbuttonsystemitem?language=objc)
-'''
-UIBarButtonSystemItemDone
-'''
-done = 0
+edgeNone = 0  # UIRectEdgeNone
+touchUpInside = 1 << 6  # UIControlEventTouchUpInside
+done = 0  # UIBarButtonSystemItemDone
+
+
+# --- present
+@onMainThread
+def present_viewController(myVC: UIViewController):
+  app = ObjCClass('UIApplication').sharedApplication
+  window = app.keyWindow if app.keyWindow else app.windows[0]
+  rootVC = window.rootViewController
+
+  while _presentedVC := rootVC.presentedViewController:
+    rootVC = _presentedVC
+
+  myNC = RootNavigationController.alloc().initWithRootViewController_(myVC)
+
+  presentVC = myNC
+  '''
+  UIModalPresentationFullScreen = 0
+  UIModalPresentationPageSheet = 1
+  '''
+  presentVC.setModalPresentationStyle_(1)
+
+  rootVC.presentViewController_animated_completion_(presentVC, True, None)
 
 
 # --- NavigationController
@@ -75,112 +85,28 @@ class RootNavigationController(UINavigationController,
 
 
 # --- ViewController
-class FirstViewController(UIViewController):
-
-  @objc_method
-  def onTap_(self, sender):
-    svc = SecondViewController.new()
-    navigationController = self.navigationController
-    navigationController.pushViewController_animated_(svc, True)
+class MainViewController(UIViewController):
 
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
     # --- Navigation
-    self.navigationItem.title = 'FirstView'
+    self.navigationItem.title = 'main'
 
     # --- View
     self.view.backgroundColor = UIColor.systemBlueColor()
-    config = UIButtonConfiguration.tintedButtonConfiguration()
-    config.title = 'Tap'
-    config.baseBackgroundColor = UIColor.systemPinkColor()
-    config.baseForegroundColor = UIColor.systemGreenColor()
-
-    tapButton = UIButton.new()
-    tapButton.configuration = config
-    tapButton.addTarget_action_forControlEvents_(self, SEL('onTap:'),
-                                                 touchUpInside)
-
-    self.view.addSubview_(tapButton)
-    # --- Layout
-    tapButton.translatesAutoresizingMaskIntoConstraints = False
-    NSLayoutConstraint.activateConstraints_([
-      tapButton.centerXAnchor.constraintEqualToAnchor_(
-        self.view.centerXAnchor),
-      tapButton.centerYAnchor.constraintEqualToAnchor_(
-        self.view.centerYAnchor),
-      tapButton.widthAnchor.constraintEqualToAnchor_multiplier_(
-        self.view.widthAnchor, 0.4),
-      tapButton.heightAnchor.constraintEqualToAnchor_multiplier_(
-        self.view.heightAnchor, 0.1),
-    ])
 
 
-class SecondViewController(UIViewController):
+# --- SF Symbols
 
-  @objc_method
-  def onTap_(self, sender):
-    navigationController = self.navigationController
-    navigationController.popViewControllerAnimated_(True)
+# --- UITableView
+UITableView = ObjCClass('UITableView')
+UITableViewCell = ObjCClass('UITableViewCell')
 
-  @objc_method
-  def viewDidLoad(self):
-    send_super(__class__, self, 'viewDidLoad')
-    # --- Navigation
-    self.navigationItem.title = 'SecondView'
-
-    # --- View
-    self.view.backgroundColor = UIColor.systemGreenColor()
-    config = UIButtonConfiguration.tintedButtonConfiguration()
-    config.title = 'Tap'
-    config.baseBackgroundColor = UIColor.systemPinkColor()
-    config.baseForegroundColor = UIColor.systemBlueColor()
-
-    tapButton = UIButton.new()
-    tapButton.configuration = config
-    tapButton.addTarget_action_forControlEvents_(self, SEL('onTap:'),
-                                                 touchUpInside)
-
-    self.view.addSubview_(tapButton)
-    # --- Layout
-    tapButton.translatesAutoresizingMaskIntoConstraints = False
-    NSLayoutConstraint.activateConstraints_([
-      tapButton.centerXAnchor.constraintEqualToAnchor_(
-        self.view.centerXAnchor),
-      tapButton.centerYAnchor.constraintEqualToAnchor_(
-        self.view.centerYAnchor),
-      tapButton.widthAnchor.constraintEqualToAnchor_multiplier_(
-        self.view.widthAnchor, 0.4),
-      tapButton.heightAnchor.constraintEqualToAnchor_multiplier_(
-        self.view.heightAnchor, 0.1),
-    ])
-
-
-# --- main
-@onMainThread
-def present_viewController(myVC: UIViewController):
-  app = ObjCClass('UIApplication').sharedApplication
-  window = app.keyWindow if app.keyWindow else app.windows[0]
-  rootVC = window.rootViewController
-
-  while _presentedVC := rootVC.presentedViewController:
-    rootVC = _presentedVC
-
-  myNC = RootNavigationController.alloc().initWithRootViewController_(myVC)
-
-  presentVC = myNC
-
-  # ref: [UIModalPresentationStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uimodalpresentationstyle?language=objc)
-  '''
-  UIModalPresentationFullScreen = 0
-  UIModalPresentationPageSheet = 1
-  '''
-  presentVC.setModalPresentationStyle_(1)
-
-  rootVC.presentViewController_animated_completion_(presentVC, True, None)
+# --- TableView
 
 
 if __name__ == "__main__":
-  vc = FirstViewController.new()
-  present_viewController(vc)
+  mainVC = MainViewController.new()
+  present_viewController(mainVC)
 
