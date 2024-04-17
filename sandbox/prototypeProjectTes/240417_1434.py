a8b81243cc5edaf924d7672691bf2d415107f4bb
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-04-17 14:55:11 +0900

    up

---
+++ sandbox/prototypeProjectTes/240417_1434.py
@@ -0,0 +1,118 @@
+from pyrubicon.objc.api import ObjCClass, ObjCProtocol, objc_method
+from pyrubicon.objc.runtime import SEL, send_super, libc, libobjc,load_library,Foundation
+
+from mainThread import onMainThread
+import pdbr
+
+ObjCClass.auto_rename = True
+NSStringFromClass = load_library('NSStringFromClass')
+
+# --- UINavigationController
+UINavigationController = ObjCClass('UINavigationController')
+UINavigationControllerDelegate = ObjCProtocol('UINavigationControllerDelegate')
+UINavigationBarAppearance = ObjCClass('UINavigationBarAppearance')
+UIBarButtonItem = ObjCClass('UIBarButtonItem')
+
+# --- UIViewController
+UIViewController = ObjCClass('UIViewController')
+UIColor = ObjCClass('UIColor')
+UIButtonConfiguration = ObjCClass('UIButtonConfiguration')
+UIButton = ObjCClass('UIButton')
+
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+# ref: [UIRectEdge | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uirectedge?language=objc)
+'''
+UIRectEdgeNone = 0
+'''
+edgeNone = 0
+
+# ref: [UIControlEvents | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uicontrolevents?language=objc)
+'''
+UIControlEventTouchUpInside = 1 <<  6
+'''
+touchUpInside = 1 << 6
+
+# ref: [UIBarButtonSystemItem | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uibarbuttonsystemitem?language=objc)
+'''
+UIBarButtonSystemItemDone
+'''
+done = 0
+
+# --- present
+@onMainThread
+def present_viewController(myVC: UIViewController):
+  app = ObjCClass('UIApplication').sharedApplication
+  window = app.keyWindow if app.keyWindow else app.windows[0]
+  rootVC = window.rootViewController
+
+  while _presentedVC := rootVC.presentedViewController:
+    rootVC = _presentedVC
+
+  myNC = RootNavigationController.alloc().initWithRootViewController_(myVC)
+
+  presentVC = myNC
+  '''
+  UIModalPresentationFullScreen = 0
+  UIModalPresentationPageSheet = 1
+  '''
+  presentVC.setModalPresentationStyle_(0)
+
+  rootVC.presentViewController_animated_completion_(presentVC, True, None)
+
+
+
+# --- NavigationController
+class RootNavigationController(UINavigationController,
+                               protocols=[UINavigationControllerDelegate]):
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    appearance = UINavigationBarAppearance.new()
+    appearance.configureWithDefaultBackground()
+
+    navigationBar = self.navigationBar
+    navigationBar.standardAppearance = appearance
+    navigationBar.scrollEdgeAppearance = appearance
+    navigationBar.compactAppearance = appearance
+    navigationBar.compactScrollEdgeAppearance = appearance
+
+    self.delegate = self
+
+  @objc_method
+  def doneButtonTapped_(self, sender):
+    visibleViewController = self.visibleViewController
+    visibleViewController.dismissViewControllerAnimated_completion_(True, None)
+
+  @objc_method
+  def navigationController_willShowViewController_animated_(
+      self, navigationController, viewController, animated: bool):
+    viewController.setEdgesForExtendedLayout_(edgeNone)
+    doneButton = UIBarButtonItem.alloc(
+    ).initWithBarButtonSystemItem_target_action_(done, navigationController,
+                                                 SEL('doneButtonTapped:'))
+    visibleViewController = navigationController.visibleViewController
+
+    navigationItem = visibleViewController.navigationItem
+    navigationItem.rightBarButtonItem = doneButton
+
+
+# --- ViewController
+class FirstViewController(UIViewController):
+
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    # --- Navigation
+    self.navigationItem.title = 'FirstView'
+
+    # --- View
+    self.view.backgroundColor = UIColor.systemBlueColor()
+    
+
+if __name__ == "__main__":
+  vc = FirstViewController.new()
+  present_viewController(vc)
+
+
