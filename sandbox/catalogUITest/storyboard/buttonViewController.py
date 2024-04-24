58486211d8fe5a29cae10410dfd2b7eda50348a7
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-04-24 14:37:11 +0900

    up

--- sandbox/catalogUITest/storyboard/buttonViewController.py
+++ sandbox/catalogUITest/storyboard/buttonViewController.py
@@ -1,4 +1,16 @@
 from pyrubicon.objc.api import ObjCClass, objc_method
 from pyrubicon.objc.runtime import send_super
 
-prototypes = []
+UITableViewCell = ObjCClass('UITableViewCell')
+
+
+class ButtonSystemAddContact(UITableViewCell):
+
+  @objc_method
+  def initWithStyle_reuseIdentifier_(self, style, reuseIdentifier):
+    cell = send_super(__class__, self, 'initWithStyle:reuseIdentifier:')
+    return cell
+
+
+prototypes = [ButtonSystemAddContact,]
+
