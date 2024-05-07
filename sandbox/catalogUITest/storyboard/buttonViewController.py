9499e08f897e0adcfd2805c21aba18801570c593
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-05-07 16:09:21 +0900

    up

--- sandbox/catalogUITest/storyboard/buttonViewController.py
+++ sandbox/catalogUITest/storyboard/buttonViewController.py
@@ -1,7 +1,11 @@
 from pyrubicon.objc.api import ObjCClass, objc_method
 from pyrubicon.objc.runtime import send_super
 
+
+ObjCClass.auto_rename = True # xxx: ここ含めて全部呼び出し？
+
 UITableViewCell = ObjCClass('UITableViewCell')
+
 
 
 class ButtonSystemAddContact(UITableViewCell):
