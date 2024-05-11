7cb4953825f2397e2187314c7fd93d30b9dd15f9
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-05-11 15:37:54 +0900

    run test

--- sandbox/tableTest/240511_1534.py
+++ sandbox/tableTest/240511_1534.py
@@ -1,1 +1,55 @@
+# [pystaRubiconObjcSandBox/sandbox/catalogUITest/_main.py at main · pome-ta/pystaRubiconObjcSandBox · GitHub](https://github.com/pome-ta/pystaRubiconObjcSandBox/blob/main/sandbox/catalogUITest/_main.py)
+
+from pyrubicon.objc.api import ObjCClass, objc_method
+from pyrubicon.objc.runtime import send_super
+
+from rbedge.functions import NSStringFromClass
+from rbedge import pdbr
+#ObjCClass.auto_rename = True
+
+# --- UIViewController
+UIViewController = ObjCClass('UIViewController')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+UILabel = ObjCClass('UILabel')
+UIFont = ObjCClass('UIFont')
+UIColor = ObjCClass('UIColor')
+
+
+class MainViewController(UIViewController):
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    # --- Navigation
+    title = NSStringFromClass(__class__)
+    self.navigationItem.title = title
+
+    # --- View
+    backgroundColor = UIColor.systemBackgroundColor()
+    self.view.backgroundColor = backgroundColor
+
+    self.label = UILabel.new()
+    self.label.text = 'UIKitCatalog'
+    self.label.font = UIFont.systemFontOfSize_(26.0)
+    self.label.sizeToFit()
+
+    self.view.addSubview_(self.label)
+
+    # --- Layout
+    self.label.translatesAutoresizingMaskIntoConstraints = False
+
+    NSLayoutConstraint.activateConstraints_([
+      self.label.centerXAnchor.constraintEqualToAnchor_(
+        self.view.centerXAnchor),
+      self.label.centerYAnchor.constraintEqualToAnchor_(
+        self.view.centerYAnchor),
+    ])
+
+
+if __name__ == '__main__':
+  from rbedge import present_viewController
+
+  vc = MainViewController.new()
+  present_viewController(vc)
 
