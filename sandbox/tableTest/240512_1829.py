81b495ab3d32d9bc5c072038aa8fb3161494aceb
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-05-13 08:43:56 +0900

    up

--- sandbox/tableTest/240512_1829.py
+++ sandbox/tableTest/240512_1829.py
@@ -1,7 +1,7 @@
 import ctypes
 
 from pyrubicon.objc.api import ObjCClass, ObjCInstance, objc_method
-from pyrubicon.objc.runtime import send_super, libobjc
+from pyrubicon.objc.runtime import send_super, libobjc,objc_super
 from pyrubicon.objc.types import NSInteger
 
 from rbedge.enumerations import UITableViewStyle
@@ -16,20 +16,29 @@ UITableView = ObjCClass('UITableView')  # todo: 型ヒント
 
 UIColor = ObjCClass('UIColor')
 
+#libobjc.class_getSuperclass.restype = ctypes.c_void_p
+#libobjc.class_getSuperclass.argtypes = [ctypes.c_void_p]
 
+#print(libobjc.class_getSuperclass)
 class TableViewControllerTest(UITableViewController):
-  '''
+
   @objc_method
   def init(self):
     send_super(__class__, self, 'init')
     self.cell_identifier = 'customCell'
-    return self
-
-  '''
-
-  @objc_method
-  def initWithStyle_(self, style: ctypes.c_void_p):
-    self = send_super(__class__, self, 'initWithStyle:')
+    #print(dir(libobjc.class_getSuperclass))
+    #print(libobjc.class_getSuperclass.argtypes)
+    #print(dir(self))
+    #print(dir(self.objc_class))
+    #print(self)
+    #print(self.objc_class)
+    #s = libobjc.class_getSuperclass(Class(objc_id(self)))
+    #s = Class(self.objc_class)
+    #print(__class__._as_parameter_)
+    super_ptr = libobjc.class_getSuperclass(__class__._as_parameter_)
+    super_struct = objc_super(self, super_ptr)
+    print(super_struct)
+    
     return self
 
   @objc_method
