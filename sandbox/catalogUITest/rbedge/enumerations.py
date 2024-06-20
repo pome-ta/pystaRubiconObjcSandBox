220a5d2e5a80b4136a2e555193950472da93bfc6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-06-20 17:19:19 +0900

    UICollectionLayoutListConfiguration

--- sandbox/catalogUITest/rbedge/enumerations.py
+++ sandbox/catalogUITest/rbedge/enumerations.py
@@ -1,38 +1,9 @@
 from dataclasses import dataclass
 
 
-@dataclass
-class UIRectEdge:
-  # ref: [UIRectEdge | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uirectedge?language=objc)
-  none: int = 0
-  top: int = 1 << 0
-  left: int = 1 << 1
-  bottom: int = 1 << 2
-  right: int = 1 << 3
-  all: int = top | left | bottom | right
-
-
-@dataclass
-class UIView_ContentMode:
-  # ref: [UIView.ContentMode | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uiview/contentmode)
-  scaleToFill: int = 0
-  scaleAspectFit: int = 1
-  scaleAspectFill: int = 2
-  redraw: int = 3
-  center: int = 4
-  top: int = 5
-  bottom: int = 6
-  left: int = 7
-  right: int = 8
-  topLeft: int = 9
-  topRight: int = 10
-  bottomLeft: int = 11
-  bottomRight: int = 12
-
-
+# ref: [UIModalPresentationStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uimodalpresentationstyle)
 @dataclass
 class UIModalPresentationStyle:
-  # ref: [UIModalPresentationStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uimodalpresentationstyle)
   automatic: int = -2
   none: int = -1
   fullScreen: int = 0
@@ -46,52 +17,43 @@ class UIModalPresentationStyle:
   blurOverFullScreen: int = 8
 
 
+# ref: [UIRectEdge | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uirectedge?language=objc)
 @dataclass
-class UITableViewCell_AccessoryType:
-  # ref: [UITableViewCell.AccessoryType | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uitableviewcell/accessorytype)
+class UIRectEdge:
   none: int = 0
-  disclosureIndicator: int = 1
-  detailDisclosureButton: int = 2
-  checkmark: int = 3
-  detailButton: int = 4
+  top: int = 1 << 0
+  left: int = 1 << 1
+  bottom: int = 1 << 2
+  right: int = 1 << 3
+  all: int = top | left | bottom | right
 
 
+# ref: [UIBarButtonSystemItem | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uibarbuttonsystemitem?language=objc)
 @dataclass
-class UIBarButtonItem_SystemItem:
-  # ref: [UIBarButtonSystemItem | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uibarbuttonsystemitem?language=objc)
-  done = 0
-  cancel = 1
-  edit = 2
-  add = 4
-  flexibleSpace = 5
-  fixedSpace = 6
-  compose = 7
-  reply = 8
-  action = 9
-  organize = 10
-  bookmarks = 11
-  search = 12
-  refresh = 13
-  stop = 14
-  trash = 16
-  play = 17
-  pause = 18
-  rewind = 19
-  fastForward = 20
-  undo = 21
-  redo = 22
-  pageCurl = 23  # Deprecated
-  close = 24
-
-
-@dataclass
-class NSTextAlignment:
-  # ref: [NSTextAlignment | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/nstextalignment?language=objc)
-  left = 0
-  right = 2
-  center = 1
-  justified = 3
-  natural = 4
+class UIBarButtonSystemItem:
+  done: int = 0
+  cancel: int = 1
+  edit: int = 2
+  add: int = 4
+  flexibleSpace: int = 5
+  fixedSpace: int = 6
+  compose: int = 7
+  reply: int = 8
+  action: int = 9
+  organize: int = 10
+  bookmarks: int = 11
+  search: int = 12
+  refresh: int = 13
+  stop: int = 14
+  trash: int = 16
+  play: int = 17
+  pause: int = 18
+  rewind: int = 19
+  fastForward: int = 20
+  undo: int = 21
+  redo: int = 22
+  pageCurl: int = 23  # Deprecated
+  close: int = 24
 
 
 # ref: [UIButtonType | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uibuttontype?language=objc)
@@ -107,122 +69,176 @@ class UIButtonType:
   close: int = 7
 
 
+# ref: [UIControlState | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uicontrolstate?language=objc)
 @dataclass
-class UIControl_State:
-  # ref: [UIControlState | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uicontrolstate?language=objc)
-  normal = 0
-  highlighted = 1 << 0
-  disabled = 1 << 1
-  selected = 1 << 2
-  focused = 1 << 3
-  application = 0x00FF0000
-  reserved = 0xFF000000
+class UIControlState:
+  normal: int = 0
+  highlighted: int = 1 << 0
+  disabled: int = 1 << 1
+  selected: int = 1 << 2
+  focused: int = 1 << 3
+  application: int = 0x00FF0000
+  reserved: int = 0xFF000000
 
 
+# ref: [UIControlEvents | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uicontrolevents?language=objc)
 @dataclass
-class UIControl_Event:
-  # ref: [UIControlEvents | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uicontrolevents?language=objc)
-  touchDown = 1 << 0
-  touchDownRepeat = 1 << 1
-  touchDragInside = 1 << 2
-  touchDragOutside = 1 << 3
-  touchDragEnter = 1 << 4
-  touchDragExit = 1 << 5
-  touchUpInside = 1 << 6
-  touchUpOutside = 1 << 7
-  touchCancel = 1 << 8
-  valueChanged = 1 << 12
-  menuActionTriggered = 1 << 14
-  primaryActionTriggered = 1 << 13
-  editingDidBegin = 1 << 16
-  editingChanged = 1 << 17
-  editingDidEnd = 1 << 18
-  editingDidEndOnExit = 1 << 19
-  allTouchEvents = 0x00000FFF
-  allEditingEvents = 0x000F0000
-  applicationReserved = 0x0F000000
-  systemReserved = 0xF0000000
-  allEvents = 0xFFFFFFFF
+class UIControlEvents:
+  touchDown: int = 1 << 0
+  touchDownRepeat: int = 1 << 1
+  touchDragInside: int = 1 << 2
+  touchDragOutside: int = 1 << 3
+  touchDragEnter: int = 1 << 4
+  touchDragExit: int = 1 << 5
+  touchUpInside: int = 1 << 6
+  touchUpOutside: int = 1 << 7
+  touchCancel: int = 1 << 8
+  valueChanged: int = 1 << 12
+  menuActionTriggered: int = 1 << 14
+  primaryActionTriggered: int = 1 << 13
+  editingDidBegin: int = 1 << 16
+  editingChanged: int = 1 << 17
+  editingDidEnd: int = 1 << 18
+  editingDidEndOnExit: int = 1 << 19
+  allTouchEvents: int = 0x00000FFF
+  allEditingEvents: int = 0x000F0000
+  applicationReserved: int = 0x0F000000
+  systemReserved: int = 0xF0000000
+  allEvents: int = 0xFFFFFFFF
 
 
-@dataclass
-class UIButton_Configuration_CornerStyle:
-  # ref: [UIButtonConfigurationCornerStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uibuttonconfigurationcornerstyle?language=objc)
-  # todo: `Enumeration Case` に値表記が無いので独自に調査
-  dynamic = 0
-  fixed = -1
-  capsule = 4
-  large = 3
-  medium = 2
-  small = 1
-
-
-@dataclass
-class UIButton_Configuration_Size:
-  # ref: [UIButtonConfigurationSize | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uibuttonconfigurationsize?language=objc)
-  # todo: `Enumeration Case` に値表記が無いので独自に調査
-  medium = 0
-  small = 1
-  mini = 2
-  large = 3
-
-
-@dataclass
-class NSUnderlineStyle:
-  # ref: [NSUnderlineStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/nsunderlinestyle?language=objc)
-  none = 0x00  # ref: [NSUnderlineStyleNone | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/nsunderlinestyle/nsunderlinestylenone?language=objc)
-  single = 0x01
-  thick = 0x02
-  double = 0x09
-  patternSolid = 0x0000  # xxx: ?
-  patternDot = 0x0100
-  patternDash = 0x0200
-  patternDashDot = 0x0300
-  patternDashDotDot = 0x0400
-  byWord = 0x8000
-
-
-@dataclass
-class UIImage_SymbolScale:
-  # ref: [UIImageSymbolScale | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uiimagesymbolscale?language=objc)
-  default = -1
-  unspecified = 0
-  small = 1
-  medium = 2
-  large = 3
-
-
-@dataclass
-class NSDirectionalRectEdge:
-  # ref: [NSDirectionalRectEdge | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/nsdirectionalrectedge?language=objc)
-  none = 0  # ref: [NSDirectionalRectEdgeNone | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/nsdirectionalrectedge/nsdirectionalrectedgenone?language=objc)
-  top = 1 << 0
-  leading = 1 << 1
-  bottom = 1 << 2
-  trailing = 1 << 3
-  all = top | leading | bottom | trailing
-
-
-@dataclass
-class UITableViewStyle:
-  # ref: [UITableViewStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uitableviewstyle?language=objc)
-  plain = 0
-  grouped = 1
-  insetGrouped = 2
-
-
+# ref: [UIListContentTextAlignment | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uilistcontenttextalignment)
 @dataclass
 class UIListContentTextAlignment:
-  # ref: [UIListContentTextAlignment | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uilistcontenttextalignment)
   # todo: `Enumeration Case` に値表記が無いので独自に調査
-  natural = 0
-  center = 1
-  justified = 2
+  natural: int = 0
+  center: int = 1
+  justified: int = 2
 
 
+# ref: [UITableViewStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uitableviewstyle?language=objc)
+@dataclass
+class UITableViewStyle:
+  plain: int = 0
+  grouped: int = 1
+  insetGrouped: int = 2
+
+
+# ref: [UIButtonConfigurationCornerStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uibuttonconfigurationcornerstyle?language=objc)
+@dataclass
+class UIButtonConfigurationCornerStyle:
+  # todo: `Enumeration Case` に値表記が無いので独自に調査
+  dynamic: int = 0
+  fixed: int = -1
+  capsule: int = 4
+  large: int = 3
+  medium: int = 2
+  small: int = 1
+
+
+# ref: [UIImageRenderingMode | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uiimagerenderingmode?language=objc)
 @dataclass
 class UIImageRenderingMode:
-  automatic = 0
-  alwaysOriginal = 1
-  alwaysTemplate = 2
+  automatic: int = 0
+  alwaysOriginal: int = 1
+  alwaysTemplate: int = 2
+
+
+# ref: [NSUnderlineStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/nsunderlinestyle?language=objc)
+@dataclass
+class NSUnderlineStyle:
+  none: int = 0x00  # xxx: patternSolid ?
+  single: int = 0x01
+  thick: int = 0x02
+  double: int = 0x09
+  patternSolid: int = 0x0000  # xxx: none ?
+  patternDot: int = 0x0100
+  patternDash: int = 0x0200
+  patternDashDot: int = 0x0300
+  patternDashDotDot: int = 0x0400
+  byWord: int = 0x8000
+
+
+# ref: [UIImageSymbolScale | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uiimagesymbolscale?language=objc)
+@dataclass
+class UIImageSymbolScale:
+  default: int = -1
+  unspecified: int = 0
+  small: int = 1
+  medium: int = 2
+  large: int = 3
+
+
+# ref: [NSDirectionalRectEdge | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/nsdirectionalrectedge?language=objc)
+@dataclass
+class NSDirectionalRectEdge:
+  none: int = 0  # ref: [NSDirectionalRectEdgeNone | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/nsdirectionalrectedge/nsdirectionalrectedgenone?language=objc)
+  top: int = 1 << 0
+  leading: int = 1 << 1
+  bottom: int = 1 << 2
+  trailing: int = 1 << 3
+  all: int = top | leading | bottom | trailing
+
+
+# ref: [UIButtonConfigurationSize | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uibuttonconfigurationsize)
+@dataclass
+class UIButtonConfigurationSize:
+  # todo: `Enumeration Case` に値表記が無いので独自に調査
+  medium: int = 0
+  small: int = 1
+  mini: int = 2
+  large: int = 3
+
+
+# ref: [UIMenuElementState | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uimenuelementstate?language=objc)
+@dataclass
+class UIMenuElementState:
+  off: int = 0
+  on: int = 1
+  mixed: int = 2
+
+
+# ref: [UIMenuElementAttributes | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uimenuelementattributes)
+@dataclass
+class UIMenuElementAttributes:
+  destructive: int = 1 << 1
+  disabled: int = 1 << 0
+  hidden: int = 1 << 2
+  keepsMenuPresented: int = 1 << 3
+
+
+# ref: [UIMenuOptions | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uimenuoptions?language=objc)
+@dataclass
+class UIMenuOptions:
+  displayInline: int = 1 << 0
+  destructive: int = 1 << 1
+  singleSelection: int = 1 << 5
+  displayAsPalette: int = 1 << 7
+
+
+# ref: [UISplitViewControllerStyle | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uisplitviewcontrollerstyle?language=objc)
+@dataclass
+class UISplitViewControllerStyle:
+  doubleColumn: int = 1
+  tripleColumn: int = 2
+
+
+# ref: [UISplitViewControllerColumn | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uisplitviewcontrollercolumn?language=objc)
+@dataclass
+class UISplitViewControllerColumn:
+  primary: int = 0
+  supplementary: int = 1
+  secondary: int = 2
+  compact: int = 3
+
+
+# ref: [UICollectionLayoutListAppearance | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uicollectionlayoutlistappearance?language=objc)
+@dataclass
+class UICollectionLayoutListAppearance:
+  # xxx: 独自調査 a.k.a: 勘
+  plain: int = 0
+  grouped: int = 1
+  insetGrouped: int = 2
+  sidebar: int = 3
+  sidebarPlain: int = 4
 
