20122ca689fe1db7548e23884c26fc27ee455707
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-07-04 13:04:30 +0900

    reset

--- sandbox/catalogUITest/240703_2000.py
+++ sandbox/catalogUITest/240703_2000.py
@@ -21,24 +21,8 @@ NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
 
 # --- UICollectionView
 UICollectionView = ObjCClass('UICollectionView')
-UICollectionViewDiffableDataSource = ObjCClass(
-  'UICollectionViewDiffableDataSource')
 NSCollectionLayoutSize = ObjCClass('NSCollectionLayoutSize')
 NSCollectionLayoutDimension = ObjCClass('NSCollectionLayoutDimension')
-NSCollectionLayoutItem = ObjCClass('NSCollectionLayoutItem')
-NSCollectionLayoutGroup = ObjCClass('NSCollectionLayoutGroup')
-NSCollectionLayoutSection = ObjCClass('NSCollectionLayoutSection')
-UICollectionViewCompositionalLayout = ObjCClass(
-  'UICollectionViewCompositionalLayout')
-UICollectionViewCellRegistration = ObjCClass(
-  'UICollectionViewCellRegistration')
-UICollectionViewListCell = ObjCClass('UICollectionViewListCell')
-NSDiffableDataSourceSnapshot = ObjCClass('NSDiffableDataSourceSnapshot')
-
-UICollectionViewDelegate = ObjCProtocol('UICollectionViewDelegate')
-UICollectionViewDataSource = ObjCProtocol('UICollectionViewDataSource')
-# ---
-
 # [モダンなUICollectionViewでシンプルなリストレイアウト その1 〜 概要](https://zenn.dev/samekard_dev/articles/43991e9321b6c9)
 
 
@@ -49,14 +33,9 @@ class Section(Enum):
 prefectures = ['福岡', '佐賀', '長崎', '大分', '熊本', '宮崎', '鹿児島']
 
 
-class ViewController(UIViewController,
-                     protocols=[
-                       UICollectionViewDelegate,
-                       UICollectionViewDataSource,
-                     ]):
+class ViewController(UIViewController):
 
-  dataSource: UICollectionViewDiffableDataSource = objc_property()
-  collectionView: UICollectionView = objc_property()
+  #collectionView: UICollectionView = objc_property()
 
   @objc_method
   def viewDidLoad(self):
@@ -65,74 +44,12 @@ class ViewController(UIViewController,
     self.navigationItem.title = title
     self.view.backgroundColor = UIColor.systemDarkRedColor()
 
-    itemSize = NSCollectionLayoutSize.sizeWithWidthDimension_heightDimension_(
-      NSCollectionLayoutDimension.fractionalWidthDimension_(1.0),
-      NSCollectionLayoutDimension.fractionalHeightDimension_(1.0))
-
-    item = NSCollectionLayoutItem.itemWithLayoutSize_(itemSize)
-
-    groupSize = NSCollectionLayoutSize.sizeWithWidthDimension_heightDimension_(
-      NSCollectionLayoutDimension.fractionalWidthDimension_(1.0),
-      NSCollectionLayoutDimension.absoluteDimension_(44))
-
-    group = NSCollectionLayoutGroup.horizontalGroupWithLayoutSize_subitems_(
-      groupSize, [
-        item,
-      ])
-
-    section = NSCollectionLayoutSection.sectionWithGroup_(group)
-    section.interGroupSpacing = 0
-    section.contentInsets = NSDirectionalEdgeInsetsMake(10.0, 10.0, 10.0, 10.0)
-
-    layout = UICollectionViewCompositionalLayout.alloc().initWithSection_(
-      section)
-
-    self.collectionView = UICollectionView.alloc(
-    ).initWithFrame_collectionViewLayout_(self.view.bounds, layout)
-
-    #pdbr.state(self.collectionView)
-
-    self.collectionView.backgroundColor = UIColor.systemDarkPurpleColor()
-
-    autoresizingMask = UIViewAutoresizing.flexibleHeight | UIViewAutoresizing.flexibleWidth
-    self.collectionView.autoresizingMask = autoresizingMask
-    self.view.addSubview_(self.collectionView)
-    self.collectionView.delegate = self
-
-    @Block
-    def configurationHandler(cell: objc_id, indexPath: objc_id,
-                             identifier: objc_id) -> None:
-      print('j')
-      contentConfiguration = cell.defaultContentConfiguration()
-      contentConfiguration.text = str(identifier)
-      cell.contentConfiguration = contentConfiguration
-
-    cellRegistration = UICollectionViewCellRegistration.registrationWithCellClass_configurationHandler_(
-      UICollectionViewListCell, None)
-
-    @Block
-    def cellProvider(collectionView: objc_id, indexPath: objc_id,
-                     identifier: objc_id) -> objc_id:
-      print('h')
-      
-      return self.collectionView.dequeueConfiguredReusableCellWithRegistration_forIndexPath_item_(
-        cellRegistration, indexPath, identifier).ptr
-
-    self.dataSource = UICollectionViewDiffableDataSource.alloc(
-    ).initWithCollectionView_cellProvider_(self.collectionView, cellProvider)
-
-    snapshot = NSDiffableDataSourceSnapshot.alloc().init()
-    snapshot.appendSectionsWithIdentifiers_([Section.main])
-    snapshot.appendItemsWithIdentifiers_intoSectionWithIdentifier_(
-      prefectures, Section.main)
-
-    self.dataSource.applySnapshot_animatingDifferences_(snapshot, False)
-    #pdbr.state(self.dataSource.snapshot())
-
   @objc_method
-  def collectionView_didSelectItemAtIndexPath_(self, collectionView,
-                                               indexPath):
-    pdbr.state(collectionView)
+  def createLayout(self):
+    dimension = NSCollectionLayoutDimension
+    
+    
+    
 
 
 if __name__ == '__main__':
