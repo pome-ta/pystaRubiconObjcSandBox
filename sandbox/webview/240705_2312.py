5c2daaac7cf33c9a1d2db0482594e9eb004bdce4
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-07-06 17:59:34 +0900

    call url

--- sandbox/webview/240705_2312.py
+++ sandbox/webview/240705_2312.py
@@ -1,6 +1,6 @@
 from pyrubicon.objc.api import ObjCClass, ObjCInstance, ObjCProtocol, objc_method, objc_property
-from pyrubicon.objc.runtime import send_super, objc_id
-from pyrubicon.objc.types import NSInteger
+from pyrubicon.objc.runtime import send_super, objc_id, load_library
+from pyrubicon.objc.types import NSInteger, CGRect
 
 from rbedge.enumerations import (
   UICollectionLayoutListAppearance,
@@ -12,25 +12,42 @@ from rbedge.functions import NSStringFromClass
 UIViewController = ObjCClass('UIViewController')
 UIColor = ObjCClass('UIColor')
 NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+CoreGraphics = load_library('CoreGraphics')
+CGRectZero = CGRect.in_dll(CoreGraphics, 'CGRectZero')
 
+NSURL = ObjCClass('NSURL')
+NSURLRequest = ObjCClass('NSURLRequest')
 # --- WKWebView
 WKWebView = ObjCClass('WKWebView')
-WKWebViewConfiguration=ObjCClass('WKWebViewConfiguration')
+WKWebViewConfiguration = ObjCClass('WKWebViewConfiguration')
 
 WKUIDelegate = ObjCProtocol('WKUIDelegate')
+WKNavigationDelegate = ObjCProtocol('WKNavigationDelegate')
 
 
 class WebView(UIViewController, protocols=[WKUIDelegate]):
+
   webView: WKWebView = objc_property()
 
   @objc_method
   def loadView(self):
-    pass
-    
+    webConfiguration = WKWebViewConfiguration.new()
+    self.webView = WKWebView.alloc().initWithFrame_configuration_(
+      CGRectZero, webConfiguration)
+    self.webView.uiDelegate = self
+    self.view = self.webView
+
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')  # xxx: 不要?
-    pdbr.state(WKWebViewConfiguration)
+    title = NSStringFromClass(__class__)
+    self.navigationItem.title = title
+
+    self.view.backgroundColor = UIColor.systemDarkRedColor()
+
+    myURL = NSURL.URLWithString_('https://www.apple.com')
+    myRequest = NSURLRequest.requestWithURL_(myURL)
+    self.webView.loadRequest_(myRequest)
 
 
 if __name__ == '__main__':
