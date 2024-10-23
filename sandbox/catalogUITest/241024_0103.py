03493a6d49eccbd34507d6220ce593cd6865e7d6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-10-24 02:00:44 +0900

    up

--- sandbox/catalogUITest/241024_0103.py
+++ sandbox/catalogUITest/241024_0103.py
@@ -9,6 +9,7 @@ from pyrubicon.objc.api import ObjCClass, ObjCInstance, Block
 from pyrubicon.objc.api import objc_method, objc_property, at
 from pyrubicon.objc.runtime import send_super, objc_id
 
+#from rbedge.types import NSDirectionalEdgeInsetsMake
 from rbedge.enumerations import UICollectionLayoutListAppearance, UIViewAutoresizing
 
 from rbedge.functions import NSStringFromClass
@@ -17,6 +18,12 @@ UIViewController = ObjCClass('UIViewController')
 UICollectionView = ObjCClass('UICollectionView')
 UICollectionViewDiffableDataSource = ObjCClass(
   'UICollectionViewDiffableDataSource')
+
+NSCollectionLayoutSize = ObjCClass('NSCollectionLayoutSize')
+NSCollectionLayoutDimension = ObjCClass('NSCollectionLayoutDimension')
+NSCollectionLayoutItem = ObjCClass('NSCollectionLayoutItem')
+NSCollectionLayoutGroup = ObjCClass('NSCollectionLayoutGroup')
+NSCollectionLayoutSection = ObjCClass('NSCollectionLayoutSection')
 
 
 class ViewController(UIViewController):
@@ -31,11 +38,39 @@ class ViewController(UIViewController):
     title = NSStringFromClass(__class__)
     self.navigationItem.title = title
 
+    self.configureHierarchy()
+
   @objc_method
   def viewDidAppear_(self, animated: bool):
     # xxx: 引数不要?
     send_super(__class__, self, 'viewDidAppear:')
     #print('viewDidAppear')
+
+  @objc_method
+  def createLayout(self):
+    itemSize = NSCollectionLayoutSize.sizeWithWidthDimension_heightDimension_(
+      NSCollectionLayoutDimension.fractionalWidthDimension_(1.0),
+      NSCollectionLayoutDimension.fractionalHeightDimension_(1.0))
+    item = NSCollectionLayoutItem.itemWithLayoutSize_(itemSize)
+    groupSize = NSCollectionLayoutSize.sizeWithWidthDimension_heightDimension_(
+      NSCollectionLayoutDimension.fractionalWidthDimension_(1.0),
+      NSCollectionLayoutDimension.absoluteDimension_(44.0))
+    group = NSCollectionLayoutGroup.horizontalGroupWithLayoutSize_subitems_(
+      groupSize, at([
+        item,
+      ]))
+    section = NSCollectionLayoutSection.sectionWithGroup_(group)
+    section.setInterGroupSpacing_(0)
+    
+    #section.setContentInsets_()
+    #m = NSDirectionalEdgeInsetsMake(10.0, 10.0, 10.0, 10.0)
+    
+    pdbr.state(m)
+
+  @objc_method
+  def configureHierarchy(self):
+    #print('configureHierarchy')
+    self.createLayout()
 
 
 if __name__ == '__main__':
