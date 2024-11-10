e03e55c67b6cdc47333696234145248e7f804096
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-11-11 00:37:29 +0900

    up

--- sandbox/textFieldViewControllerTest/241111_0014.py
+++ sandbox/textFieldViewControllerTest/241111_0014.py
@@ -1,10 +1,13 @@
 from pyrubicon.objc.api import ObjCClass, objc_method
 from pyrubicon.objc.api import objc_method, objc_property
 from pyrubicon.objc.runtime import SEL, send_super, objc_id
+from pyrubicon.objc.types import CGRectMake
 
 from rbedge.functions import NSStringFromClass
+from rbedge import pdbr
 
 UIViewController = ObjCClass('UIViewController')
+UITextField = ObjCClass('UITextField')
 
 
 class ViewController(UIViewController):
@@ -12,9 +15,16 @@ class ViewController(UIViewController):
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')  # xxx: 不要?
-
     title = NSStringFromClass(__class__)
     self.navigationItem.title = title
+
+    textField = UITextField.alloc().initWithFrame_(
+      CGRectMake(77.5, 15.0, 220.0, 34.0)).autorelease()
+
+    textField.backgroundColor = ObjCClass('UIColor').systemRedColor()
+
+    self.view.addSubview_(textField)
+    pdbr.state(textField,1)
 
 
 if __name__ == '__main__':
