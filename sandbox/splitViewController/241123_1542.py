7c00fcee8e4080d96c08c7701e1abd29777a71ec
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-11-23 15:59:53 +0900

    up

---
+++ sandbox/splitViewController/241123_1542.py
@@ -0,0 +1,141 @@
+'''
+  note:
+    `UISplitViewController` と、`UITableView`
+'''
+import ctypes
+from pyrubicon.objc.api import ObjCClass, ObjCProtocol
+from pyrubicon.objc.api import objc_method
+from pyrubicon.objc.runtime import send_super
+from pyrubicon.objc.types import NSInteger
+from rbedge.enumerations import (
+  UISplitViewControllerStyle,
+  UISplitViewControllerColumn,
+)
+
+from rbedge.functions import NSStringFromClass
+from rbedge import pdbr
+
+UIViewController = ObjCClass('UIViewController')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+# --- SplitView
+UISplitViewController = ObjCClass('UISplitViewController')
+UISplitViewControllerDelegate = ObjCProtocol('UISplitViewControllerDelegate')
+
+# --- others
+UIColor = ObjCClass('UIColor')
+UILabel = ObjCClass('UILabel')
+UIFont = ObjCClass('UIFont')
+
+
+class PrimaryViewController(UIViewController):
+
+  @objc_method
+  def viewDidLoad(self):
+    # --- View
+    self.view.backgroundColor = UIColor.systemDarkTealColor()
+
+    self.label = UILabel.new()
+    self.label.text = 'Primary'
+    self.label.font = UIFont.systemFontOfSize_(26.0)
+    self.label.sizeToFit()
+
+    self.view.addSubview_(self.label)
+
+    # --- Layout
+    self.label.translatesAutoresizingMaskIntoConstraints = False
+
+    NSLayoutConstraint.activateConstraints_([
+      self.label.centerXAnchor.constraintEqualToAnchor_(
+        self.view.centerXAnchor),
+      self.label.centerYAnchor.constraintEqualToAnchor_(
+        self.view.centerYAnchor),
+    ])
+
+
+class SecondaryViewController(UIViewController):
+
+  @objc_method
+  def viewDidLoad(self):
+    # --- View
+    self.view.backgroundColor = UIColor.systemDarkRedColor()
+
+    self.label = UILabel.new()
+    self.label.text = 'Primary'
+    self.label.font = UIFont.systemFontOfSize_(26.0)
+    self.label.sizeToFit()
+
+    self.view.addSubview_(self.label)
+
+    # --- Layout
+    self.label.translatesAutoresizingMaskIntoConstraints = False
+
+    NSLayoutConstraint.activateConstraints_([
+      self.label.centerXAnchor.constraintEqualToAnchor_(
+        self.view.centerXAnchor),
+      self.label.centerYAnchor.constraintEqualToAnchor_(
+        self.view.centerYAnchor),
+    ])
+
+
+class SplitViewController(UISplitViewController,
+                          protocols=[
+                            UISplitViewControllerDelegate,
+                          ]):
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    self.delegate = self
+    
+    primary_vc = PrimaryViewController.new()
+    primary_vc.title = primary_vc.className()
+
+    secondary_vc = SecondaryViewController.new()
+
+    primary = UISplitViewControllerColumn.primary
+    secondary = UISplitViewControllerColumn.secondary
+
+    self.setViewController_forColumn_(primary_vc, primary)
+    self.setViewController_forColumn_(secondary_vc, secondary)
+
+
+class ViewController(UIViewController):
+
+  @objc_method
+  def viewDidLoad(self):
+    # --- Navigation
+    send_super(__class__, self, 'viewDidLoad')
+    title = NSStringFromClass(__class__)
+    self.navigationItem.title = title
+
+    # --- View
+    split = SplitViewController.alloc().initWithStyle_(
+      UISplitViewControllerStyle.doubleColumn)
+    self.addChildViewController_(split)
+    self.view.addSubview_(split.view)
+    split.didMoveToParentViewController_(self)
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #pdbr.state(self,1)
+    #pdbr.state(self.childViewControllers[0])
+
+
+if __name__ == '__main__':
+  from rbedge import present_viewController
+  from rbedge.enumerations import UIModalPresentationStyle
+
+  vc = ViewController.new()
+
+  style = UIModalPresentationStyle.fullScreen
+  #style = UIModalPresentationStyle.pageSheet
+  present_viewController(vc, style)
+
