4ff4c514664d5065a40098cf98981af6c883d646
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-11-23 12:09:23 +0900

    split

--- sandbox/splitViewController/241122_2317.py
+++ sandbox/splitViewController/241122_2317.py
@@ -8,23 +8,74 @@ from pyrubicon.objc.api import objc_method
 from pyrubicon.objc.runtime import send_super
 from pyrubicon.objc.types import NSInteger
 from rbedge.enumerations import (
-  UISplitViewControllerStyle, )
+  UISplitViewControllerStyle,
+  UISplitViewControllerColumn,
+)
 
 from rbedge.functions import NSStringFromClass
 from rbedge import pdbr
 
 UIViewController = ObjCClass('UIViewController')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
 
 # --- SplitView
 UISplitViewController = ObjCClass('UISplitViewController')
 UISplitViewControllerDelegate = ObjCProtocol('UISplitViewControllerDelegate')
+
+# --- others
+UIColor = ObjCClass('UIColor')
+UILabel = ObjCClass('UILabel')
+UIFont = ObjCClass('UIFont')
 
 
 class PrimaryViewController(UIViewController):
 
   @objc_method
   def viewDidLoad(self):
-    pass
+    # --- View
+    self.view.backgroundColor = UIColor.systemDarkTealColor()
+
+    self.label = UILabel.new()
+    self.label.text = 'Primary'
+    self.label.font = UIFont.systemFontOfSize_(26.0)
+    self.label.sizeToFit()
+
+    self.view.addSubview_(self.label)
+
+    # --- Layout
+    self.label.translatesAutoresizingMaskIntoConstraints = False
+
+    NSLayoutConstraint.activateConstraints_([
+      self.label.centerXAnchor.constraintEqualToAnchor_(
+        self.view.centerXAnchor),
+      self.label.centerYAnchor.constraintEqualToAnchor_(
+        self.view.centerYAnchor),
+    ])
+
+
+class SecondaryViewController(UIViewController):
+
+  @objc_method
+  def viewDidLoad(self):
+    # --- View
+    self.view.backgroundColor = UIColor.systemDarkRedColor()
+
+    self.label = UILabel.new()
+    self.label.text = 'Primary'
+    self.label.font = UIFont.systemFontOfSize_(26.0)
+    self.label.sizeToFit()
+
+    self.view.addSubview_(self.label)
+
+    # --- Layout
+    self.label.translatesAutoresizingMaskIntoConstraints = False
+
+    NSLayoutConstraint.activateConstraints_([
+      self.label.centerXAnchor.constraintEqualToAnchor_(
+        self.view.centerXAnchor),
+      self.label.centerYAnchor.constraintEqualToAnchor_(
+        self.view.centerYAnchor),
+    ])
 
 
 class SplitViewController(UISplitViewController,
@@ -53,8 +104,17 @@ class ViewController(UIViewController):
     self.addChildViewController_(split)
     self.view.addSubview_(split.view)
     split.didMoveToParentViewController_(self)
-    
-    pdbr.state(split.view)
+
+    primary_vc = PrimaryViewController.new()
+    primary_vc.title = primary_vc.className()
+
+    secondary_vc = SecondaryViewController.new()
+
+    primary = UISplitViewControllerColumn.primary
+    secondary = UISplitViewControllerColumn.secondary
+
+    split.setViewController_forColumn_(primary_vc, primary)
+    split.setViewController_forColumn_(secondary_vc, secondary)
 
   @objc_method
   def viewDidAppear_(self, animated: bool):
