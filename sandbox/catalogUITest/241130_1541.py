eb6682d61569e7b2878af11407a364294811602e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-11-30 16:01:17 +0900

    up

--- sandbox/catalogUITest/241130_1541.py
+++ sandbox/catalogUITest/241130_1541.py
@@ -1,7 +1,9 @@
 '''
   todo: 再度シンプルチャレンジ
 '''
-from pyrubicon.objc.api import ObjCClass
+import ctypes
+
+from pyrubicon.objc.api import ObjCClass, ObjCInstance
 from pyrubicon.objc.api import objc_method, objc_property
 from pyrubicon.objc.runtime import send_super, objc_id
 from pyrubicon.objc.types import CGRectMake
@@ -9,11 +11,23 @@ from pyrubicon.objc.types import CGRectMake
 from rbedge.functions import NSStringFromClass
 from rbedge import pdbr
 
+from rbedge.enumerations import (
+  UICollectionLayoutListAppearance, )
+
 UIViewController = ObjCClass('UIViewController')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+UIColor = ObjCClass('UIColor')
+
 UICollectionView = ObjCClass('UICollectionView')
+UICollectionLayoutListConfiguration = ObjCClass(
+  'UICollectionLayoutListConfiguration')
+UICollectionViewCompositionalLayout = ObjCClass(
+  'UICollectionViewCompositionalLayout')
 
 
 class CollectionViewController(UIViewController):
+  collectionView: UICollectionView = objc_property()
 
   @objc_method
   def init(self):
@@ -21,7 +35,43 @@ class CollectionViewController(UIViewController):
     cg_zero = CGRectMake(0.0, 0.0, 0.0, 0.0)
     self.collectionView = UICollectionView.alloc(
     ).initWithFrame_collectionViewLayout_(cg_zero, self.generateLayout())
-  
+    return self
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    title = NSStringFromClass(__class__)
+    self.navigationItem.title = title
+
+    # --- View
+    self.view.backgroundColor = UIColor.systemBrownColor()  # todo: 確認用
+    self.view.addSubview_(self.collectionView)
+
+    # --- Layout
+    self.collectionView.translatesAutoresizingMaskIntoConstraints = False
+    safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
+    NSLayoutConstraint.activateConstraints_([
+      self.collectionView.centerXAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.centerXAnchor),
+      self.collectionView.centerYAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.centerYAnchor),
+      self.collectionView.widthAnchor.constraintEqualToAnchor_multiplier_(
+        safeAreaLayoutGuide.widthAnchor, 1.0),
+      self.collectionView.heightAnchor.constraintEqualToAnchor_multiplier_(
+        safeAreaLayoutGuide.heightAnchor, 1.0),
+    ])
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    pdbr.state(self.collectionView)
+
   # --- private
   @objc_method
   def generateLayout(self) -> ObjCInstance:
@@ -29,13 +79,13 @@ class CollectionViewController(UIViewController):
     #_appearance = UICollectionLayoutListAppearance.plain
     listConfiguration = UICollectionLayoutListConfiguration.alloc(
     ).initWithAppearance_(_appearance)
-    _headerMode = UICollectionLayoutListHeaderMode.firstItemInSection
-    listConfiguration.headerMode = _headerMode
+
     layout = UICollectionViewCompositionalLayout.layoutWithListConfiguration_(
       listConfiguration)
     #pdbr.state(listConfiguration.appearance)
     #print(listConfiguration.appearance)
     return layout
+
 
 if __name__ == '__main__':
   from rbedge import present_viewController
