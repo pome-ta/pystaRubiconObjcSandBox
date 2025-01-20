52bc80b2a63f47431279625dc7946f50fe314ef2
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-01-20 09:46:33 +0900

    get_srgb_named_style

--- sandbox/rubiconUpdate050/rbedge/pythonProcessUtils.py
+++ sandbox/rubiconUpdate050/rbedge/pythonProcessUtils.py
@@ -1,9 +1,17 @@
 from pathlib import Path
+import json
 
 from pyrubicon.objc.api import ObjCClass, NSData
+from .enumerations import UIUserInterfaceStyle
 
 NSURL = ObjCClass('NSURL')
 UIScreen = ObjCClass('UIScreen')
+
+# ref: [iphone - Retina display and [UIImage initWithData] - Stack Overflow](https://stackoverflow.com/questions/3289286/retina-display-and-uiimage-initwithdata)
+# xxx: scale 指定これでいいのかな?
+"""デフォルトの論理的解像度に対する物理的解像度の比
+"""
+mainScreen_scale: float = UIScreen.mainScreen.scale
 
 
 def dataWithContentsOfURL(path_str: str | Path) -> NSData:
@@ -23,9 +31,39 @@ def dataWithContentsOfURL(path_str: str | Path) -> NSData:
     NSURL.fileURLWithPath_(str(path.absolute())))
 
 
-# ref: [iphone - Retina display and [UIImage initWithData] - Stack Overflow](https://stackoverflow.com/questions/3289286/retina-display-and-uiimage-initwithdata)
-# xxx: scale 指定これでいいのかな?
-"""デフォルトの論理的解像度に対する物理的解像度の比
-"""
-mainScreen_scale: float = UIScreen.mainScreen.scale
+def get_srgb_named_style(named: str,
+                         userInterfaceStyle: UIUserInterfaceStyle) -> list:
+  # todo: 本来`UIColor.colorNamed:` で呼び出す。asset(bundle) の取り込みが難しそうなので、独自に直で呼び出し
+  _path = Path(
+    f'./UIKitCatalogCreatingAndCustomizingViewsAndControls/UIKitCatalog/Assets.xcassets/{named}.colorset/Contents.json'
+  )
+  _str = _path.read_text()
+  _dict = json.loads(_str)
+
+  def _pick_color(colors: list[dict], style: str | None = None) -> list:
+    components: dict
+    for color in colors:
+      if color.get('idiom') != 'universal':
+        continue
+      appearance, *_ = appearances if (
+        appearances := color.get('appearances')) is not None else [None]
+      if style is None and appearance is None:
+        components = color.get('color').get('components')
+        break
+      if appearance is not None and style == appearance.get('value'):
+        components = color.get('color').get('components')
+        break
+
+    red, green, blue, alpha = (float(components.get(clr))
+                               for clr in ('red', 'green', 'blue', 'alpha'))
+    # wip: エラーハンドリング
+    return [red, green, blue, alpha]
+
+  color_dicts = _dict.get('colors')
+  if userInterfaceStyle == UIUserInterfaceStyle.light:
+    return _pick_color(color_dicts, 'light')
+  elif userInterfaceStyle == UIUserInterfaceStyle.dark:
+    return _pick_color(color_dicts, 'dark')
+  else:
+    return _pick_color(color_dicts)
 
