97c49dcb2b8ca50405b77d7f5a9e3236e582f15b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-01-25 21:57:43 +0900

    UICollectionViewController

--- sandbox/diffableDataSource/250125_1534.py
+++ sandbox/diffableDataSource/250125_1534.py
@@ -1,3 +1,127 @@
 """
   note: `UICollectionViewController` でやってみる
 """
+
+import ctypes
+
+from pyrubicon.objc.api import ObjCClass, Block
+from pyrubicon.objc.api import objc_method
+from pyrubicon.objc.runtime import send_super, objc_id
+
+from rbedge.enumerations import (
+  UICollectionLayoutListAppearance, )
+
+from rbedge import pdbr
+
+UICollectionViewController = ObjCClass('UICollectionViewController')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+UIColor = ObjCClass('UIColor')
+
+UICollectionView = ObjCClass('UICollectionView')
+UICollectionLayoutListConfiguration = ObjCClass(
+  'UICollectionLayoutListConfiguration')
+UICollectionViewCompositionalLayout = ObjCClass(
+  'UICollectionViewCompositionalLayout')
+UICollectionViewDiffableDataSource = ObjCClass(
+  'UICollectionViewDiffableDataSource')
+UICollectionViewCellRegistration = ObjCClass(
+  'UICollectionViewCellRegistration')
+UICollectionViewListCell = ObjCClass('UICollectionViewListCell')
+NSDiffableDataSourceSnapshot = ObjCClass('NSDiffableDataSourceSnapshot')
+
+
+class ModernCollectionViewViewController(UICollectionViewController):
+
+  @objc_method
+  def dealloc(self):
+    # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
+    print('\tdealloc')
+    pass
+
+  @objc_method
+  def initWithCollectionViewLayout_(self, layout: objc_id):
+    send_super(__class__,
+               self,
+               'initWithCollectionViewLayout:',
+               layout,
+               restype=objc_id,
+               argtypes=[
+                 objc_id,
+               ])
+    return self
+
+  # MARK: - View Life Cycle
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    # xxx: `collectionView` での関数名衝突回避
+  @objc_method
+  def viewWillAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print('viewWillAppear')
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print('viewDidAppear')
+    pdbr.state(self.dataSource())
+
+  @objc_method
+  def viewWillDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print('viewWillDisappear')
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print('viewDidDisappear')
+
+  @objc_method
+  def didReceiveMemoryWarning(self):
+    send_super(__class__, self, 'didReceiveMemoryWarning')
+    print(f'{__class__}: didReceiveMemoryWarning')
+
+
+if __name__ == '__main__':
+  from rbedge.functions import NSStringFromClass
+  from rbedge.enumerations import UIModalPresentationStyle
+  from rbedge import present_viewController
+
+  _appearance = UICollectionLayoutListAppearance.plain
+  configuration = UICollectionLayoutListConfiguration.alloc(
+  ).initWithAppearance_(_appearance)
+  layout = UICollectionViewCompositionalLayout.layoutWithListConfiguration_(
+    configuration)
+
+  main_vc = ModernCollectionViewViewController.alloc(
+  ).initWithCollectionViewLayout_(layout)
+  _title = NSStringFromClass(ModernCollectionViewViewController)
+  main_vc.navigationItem.title = _title
+
+  presentation_style = UIModalPresentationStyle.fullScreen
+  present_viewController(main_vc, presentation_style)
+
