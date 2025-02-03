9379e1451ab4e1c23206443449420b7aa56a0fc5
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-02-04 00:50:50 +0900

    up

--- sandbox/asyncioEventloop/mainApp.py
+++ sandbox/asyncioEventloop/mainApp.py
@@ -2,11 +2,130 @@ import ctypes
 
 from pyrubicon.objc.api import ObjCClass
 from pyrubicon.objc.api import objc_method
-from pyrubicon.objc.runtime import send_super
+from pyrubicon.objc.runtime import send_super, SEL
 
 from rbedge.functions import NSStringFromClass
+from rbedge import pdbr
 
 UIViewController = ObjCClass('UIViewController')
+
+UIColor = ObjCClass('UIColor')
+UIButtonConfiguration = ObjCClass('UIButtonConfiguration')
+UIButton = ObjCClass('UIButton')
+
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+touchUpInside = 1 << 6
+
+
+class SecondViewController(UIViewController):
+
+  @objc_method
+  def dealloc(self):
+    # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
+    print(f'\t - {NSStringFromClass(__class__)}: dealloc')
+    #self.tapButton.release()
+    #self.tapButton = None
+
+  @objc_method
+  def loadView(self):
+    send_super(__class__, self, 'loadView')
+    print(f'\t{NSStringFromClass(__class__)}: loadView')
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    print(f'\t{NSStringFromClass(__class__)}: viewDidLoad')
+    self.navigationItem.title = NSStringFromClass(__class__)
+    
+    config = UIButtonConfiguration.tintedButtonConfiguration()
+    config.title = 'Tap'
+    config.baseBackgroundColor = UIColor.systemPinkColor()
+    config.baseForegroundColor = UIColor.systemBlueColor()
+
+    tapButton = UIButton.new()
+    tapButton.configuration = config
+    tapButton.addTarget_action_forControlEvents_(self, SEL('onTap:'),
+                                                 touchUpInside)
+
+    self.view.addSubview_(tapButton)
+    # --- Layout
+    tapButton.translatesAutoresizingMaskIntoConstraints = False
+    NSLayoutConstraint.activateConstraints_([
+      tapButton.centerXAnchor.constraintEqualToAnchor_(
+        self.view.centerXAnchor),
+      tapButton.centerYAnchor.constraintEqualToAnchor_(
+        self.view.centerYAnchor),
+      tapButton.widthAnchor.constraintEqualToAnchor_multiplier_(
+        self.view.widthAnchor, 0.4),
+      tapButton.heightAnchor.constraintEqualToAnchor_multiplier_(
+        self.view.heightAnchor, 0.1),
+    ])
+    
+    self.tapButton = tapButton
+    #tapButton.release()
+    
+
+  @objc_method
+  def viewWillAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    print(f'\t{NSStringFromClass(__class__)}: viewWillAppear_')
+    #pdbr.state(self.tapButton, 1)
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print(f'\t{NSStringFromClass(__class__)}: viewDidAppear_')
+    #print('\t↓ ---')
+
+  @objc_method
+  def viewWillDisappear_(self, animated: bool):
+    #print('\t↑ ---')
+    send_super(__class__,
+               self,
+               'viewWillDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print(f'\t{NSStringFromClass(__class__)}: viewWillDisappear_')
+    #self.tapButton = None
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    print(f'\t{NSStringFromClass(__class__)}: viewDidDisappear_')
+    self.tapButton = None
+    
+    
+
+  @objc_method
+  def didReceiveMemoryWarning(self):
+    send_super(__class__, self, 'didReceiveMemoryWarning')
+    print(f'\t{NSStringFromClass(__class__)}: didReceiveMemoryWarning')
+    
+  @objc_method
+  def onTap_(self, sender):
+    #navigationController = self.navigationController
+    self.navigationController.popViewControllerAnimated_(True)
 
 
 class MainViewController(UIViewController):
@@ -26,6 +145,30 @@ class MainViewController(UIViewController):
     send_super(__class__, self, 'viewDidLoad')
     print(f'\t{NSStringFromClass(__class__)}: viewDidLoad')
     self.navigationItem.title = NSStringFromClass(__class__)
+    
+    config = UIButtonConfiguration.tintedButtonConfiguration()
+    config.title = 'Tap'
+    config.baseBackgroundColor = UIColor.systemPinkColor()
+    config.baseForegroundColor = UIColor.systemGreenColor()
+
+    tapButton = UIButton.new()
+    tapButton.configuration = config
+    tapButton.addTarget_action_forControlEvents_(self, SEL('onTap:'),
+                                                 touchUpInside)
+
+    self.view.addSubview_(tapButton)
+    # --- Layout
+    tapButton.translatesAutoresizingMaskIntoConstraints = False
+    NSLayoutConstraint.activateConstraints_([
+      tapButton.centerXAnchor.constraintEqualToAnchor_(
+        self.view.centerXAnchor),
+      tapButton.centerYAnchor.constraintEqualToAnchor_(
+        self.view.centerYAnchor),
+      tapButton.widthAnchor.constraintEqualToAnchor_multiplier_(
+        self.view.widthAnchor, 0.4),
+      tapButton.heightAnchor.constraintEqualToAnchor_multiplier_(
+        self.view.heightAnchor, 0.1),
+    ])
 
   @objc_method
   def viewWillAppear_(self, animated: bool):
@@ -47,12 +190,12 @@ class MainViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    print(f'\t{NSStringFromClass(__class__)}: viewDidAppear_')
-    print('\t↓ ---')
+    #print(f'\t{NSStringFromClass(__class__)}: viewDidAppear_')
+    #print('\t↓ ---')
 
   @objc_method
   def viewWillDisappear_(self, animated: bool):
-    print('\t↑ ---')
+    #print('\t↑ ---')
     send_super(__class__,
                self,
                'viewWillDisappear:',
@@ -60,7 +203,7 @@ class MainViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    print(f'\t{NSStringFromClass(__class__)}: viewWillDisappear_')
+    #print(f'\t{NSStringFromClass(__class__)}: viewWillDisappear_')
 
   @objc_method
   def viewDidDisappear_(self, animated: bool):
@@ -77,6 +220,12 @@ class MainViewController(UIViewController):
   def didReceiveMemoryWarning(self):
     send_super(__class__, self, 'didReceiveMemoryWarning')
     print(f'\t{NSStringFromClass(__class__)}: didReceiveMemoryWarning')
+    
+  @objc_method
+  def onTap_(self, sender):
+    svc = SecondViewController.new()
+    navigationController = self.navigationController
+    navigationController.pushViewController_animated_(svc, True)
 
 
 if __name__ == '__main__':
