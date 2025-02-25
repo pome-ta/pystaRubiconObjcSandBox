390aa4d350ea54882fccced0c574be3953fce48e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-02-25 14:36:49 +0900

    up

--- sandbox/templateUIViewProject/singleFileMinimum.py
+++ sandbox/templateUIViewProject/singleFileMinimum.py
@@ -1,9 +1,39 @@
 import ctypes
 
-from pyrubicon.objc.api import ObjCClass, ObjCInstance
-from pyrubicon.objc.runtime import objc_id, send_super
+from pyrubicon.objc.api import ObjCClass, ObjCInstance, Block
+from pyrubicon.objc.api import objc_method
+from pyrubicon.objc.runtime import objc_id, send_super, SEL
+
+from rbedge import pdbr
 
 ObjCClass.auto_rename = True
+
+#############################################################
+# --- utils
+#############################################################
+from pyrubicon.objc.runtime import Class, Foundation
+
+
+def NSStringFromClass(cls: Class) -> ObjCInstance:
+  _NSStringFromClass = Foundation.NSStringFromClass
+  _NSStringFromClass.restype = ctypes.c_void_p
+  _NSStringFromClass.argtypes = [Class]
+  return ObjCInstance(_NSStringFromClass(cls))
+
+
+#############################################################
+# --- lifeCycle
+#############################################################
+
+import asyncio
+import logging
+
+from pyrubicon.objc.eventloop import EventLoopPolicy
+
+#logging.basicConfig(level=logging.DEBUG)
+asyncio.set_event_loop_policy(EventLoopPolicy())
+loop = asyncio.new_event_loop()
+#loop.set_debug(True)
 
 #############################################################
 # --- mainThread
@@ -166,4 +196,109 @@ class MainViewController(UIViewController):
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
     print(f'\t{NSStringFromClass(__class__)}: viewDidLoad')
-    self.navigationItem.title = 'title'
+    self.navigationItem.title = NSStringFromClass(__class__)
+
+  @objc_method
+  def viewWillAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    print(f'\t{NSStringFromClass(__class__)}: viewWillAppear_')
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    print(f'\t{NSStringFromClass(__class__)}: viewDidAppear_')
+    print('\t↓ ---')
+
+  @objc_method
+  def viewWillDisappear_(self, animated: bool):
+    print('\t↑ ---')
+    send_super(__class__,
+               self,
+               'viewWillDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    print(f'\t{NSStringFromClass(__class__)}: viewWillDisappear_')
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    print(f'\t{NSStringFromClass(__class__)}: viewDidDisappear_')
+
+  @objc_method
+  def didReceiveMemoryWarning(self):
+    send_super(__class__, self, 'didReceiveMemoryWarning')
+    print(f'\t{NSStringFromClass(__class__)}: didReceiveMemoryWarning')
+
+
+#############################################################
+# --- app present
+#############################################################
+UIApplication = ObjCClass('UIApplication')
+
+
+
+class App:
+
+  def __init__(self, viewController):
+    self.viewController = viewController
+    self.rootViewController = None
+
+  def main_loop(self, modalPresentationStyle: int = 0):
+    #print('App: main_loop')
+
+    sharedApplication = UIApplication.sharedApplication
+    connectedScenes = sharedApplication.connectedScenes
+    objectEnumerator = connectedScenes.objectEnumerator()
+    while (windowScene := objectEnumerator.nextObject()):
+      if windowScene.activationState == 0:
+        break
+    keyWindow = windowScene.keyWindow
+    self.rootViewController = keyWindow.rootViewController
+
+    @onMainThread
+    def present_viewController(viewController: UIViewController,
+                               _style: int) -> None:
+
+      presentViewController = RootNavigationController.alloc(
+      ).initWithRootViewController_(viewController)
+
+      presentViewController.setModalPresentationStyle_(_style)
+
+      self.rootViewController.presentViewController_animated_completion_(
+        presentViewController, True, None)
+
+    
+
+    present_viewController(self.viewController, modalPresentationStyle)
+    loop.run_forever()
+    loop.close()
+
+
+if __name__ == '__main__':
+  print('--- run')
+  main_vc = MainViewController.new()
+  presentation_style = 1
+  
+  app = App(main_vc)
+  app.main_loop(presentation_style)
+
