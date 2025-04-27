b9e913cd97e2c1cc1c904cfdf0809d3270a73df1
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-27 15:43:39 +0900

    FonTextStylesController

--- sandbox/fontTextStyles/mainApp.py
+++ sandbox/fontTextStyles/mainApp.py
@@ -9,6 +9,14 @@ from rbedge.enumerations import UITableViewStyle
 from rbedge.functions import NSStringFromClass
 from rbedge import pdbr
 
+# --- UIFontTextStyle
+from pyrubicon.objc.runtime import load_library
+from pyrubicon.objc.api import objc_const
+
+UIKit = load_library('UIKit')
+# xxx: PEP8では非推奨
+UIFontTextStyle = lambda const_name: str(objc_const(UIKit, const_name))
+
 UIViewController = ObjCClass('UIViewController')
 NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
 
@@ -18,6 +26,8 @@ UITableViewCell = ObjCClass('UITableViewCell')
 UITableViewController = ObjCClass('UITableViewController')
 UITableViewHeaderFooterView = ObjCClass('UITableViewHeaderFooterView')
 UIListContentConfiguration = ObjCClass('UIListContentConfiguration')
+
+UIFont = ObjCClass('UIFont')
 
 fontTextStyles = [
   'UIFontTextStyleBody',
@@ -42,10 +52,8 @@ cells = [[style, [
   lipsum,
 ]] for style in fontTextStyles]
 
-print(len(cells))
 
-
-class FonTextStylesTableViewController(UIViewController):
+class FonTextStylesController(UIViewController):
 
   cellIdentifier: str = objc_property()
   headerFooterViewIdentifier: str = objc_property()
@@ -67,22 +75,92 @@ class FonTextStylesTableViewController(UIViewController):
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')  # xxx: 不要?
     #print(f'\t\t{NSStringFromClass(__class__)}: viewDidLoad')
+    # --- Navigation
+    self.navigationItem.title = NSStringFromClass(__class__) if (
+      title := self.navigationItem.title) is None else title
 
     # --- Table set
     tableView = UITableView.alloc().initWithFrame_style_(
       self.view.bounds, UITableViewStyle.grouped)
+
+    tableView.registerClass_forCellReuseIdentifier_(UITableViewCell,
+                                                    self.cellIdentifier)
+    tableView.registerClass_forHeaderFooterViewReuseIdentifier_(
+      UITableViewHeaderFooterView, self.headerFooterViewIdentifier)
+
+    tableView.delegate = self
+    tableView.dataSource = self
+
+    # --- Layout
+    self.view.addSubview_(tableView)
+    tableView.translatesAutoresizingMaskIntoConstraints = False
+    # areaLayoutGuide = self.view.safeAreaLayoutGuide
+    areaLayoutGuide = self.view
+    NSLayoutConstraint.activateConstraints_([
+      tableView.centerXAnchor.constraintEqualToAnchor_(
+        areaLayoutGuide.centerXAnchor),
+      tableView.centerYAnchor.constraintEqualToAnchor_(
+        areaLayoutGuide.centerYAnchor),
+      tableView.widthAnchor.constraintEqualToAnchor_multiplier_(
+        areaLayoutGuide.widthAnchor, 1.0),
+      tableView.heightAnchor.constraintEqualToAnchor_multiplier_(
+        areaLayoutGuide.heightAnchor, 1.0),
+    ])
+
+  @objc_method
+  def didReceiveMemoryWarning(self):
+    send_super(__class__, self, 'didReceiveMemoryWarning')
+    print(f'{__class__}: didReceiveMemoryWarning')
+
+  # --- UITableViewDataSource
+  @objc_method
+  def numberOfSectionsInTableView_(self, tableView) -> int:
+    return len(cells)
+
+  '''
+  @objc_method
+  def tableView_titleForHeaderInSection_(self, tableView, section: int):
+    return cells[section][0]
+  '''
+
+  @objc_method
+  def tableView_viewForHeaderInSection_(self, tableView, section: int):
+
+    header = tableView.dequeueReusableHeaderFooterViewWithIdentifier_(
+      self.headerFooterViewIdentifier)
+    #header.textLabel.text = cells[section][0]
+    header.text = cells[section][0]
+    return header
+
+  @objc_method
+  def tableView_numberOfRowsInSection_(self, tableView, section: int) -> int:
+    return len(cells[section])
+
+  @objc_method
+  def tableView_cellForRowAtIndexPath_(self, tableView, indexPath):
+    cell = tableView.dequeueReusableCellWithIdentifier_forIndexPath_(
+      self.cellIdentifier, indexPath)
+
+    style = cells[indexPath.section][0]
+    text = cells[indexPath.section][1][indexPath.row]
+
+    contentConfiguration = cell.defaultContentConfiguration()
+    contentConfiguration.text = text
+    contentConfiguration.textProperties.setFont_(
+      UIFont.preferredFontForTextStyle_(UIFontTextStyle(style)))
+
+    cell.contentConfiguration = contentConfiguration
+    return cell
 
 
 if __name__ == '__main__':
   from rbedge.app import App
   from rbedge.enumerations import UIModalPresentationStyle
 
-  main_vc = FonTextStylesTableViewController.new()
-  _title = NSStringFromClass(FonTextStylesTableViewController)
-  main_vc.navigationItem.title = _title
+  main_vc = FonTextStylesController.new()
 
-  # presentation_style = UIModalPresentationStyle.fullScreen
-  presentation_style = UIModalPresentationStyle.pageSheet
+  presentation_style = UIModalPresentationStyle.fullScreen
+  #presentation_style = UIModalPresentationStyle.pageSheet
 
   app = App(main_vc, presentation_style)
   app.present()
