bbb5f35d47e25ba90d909c1dbe90726683c4e896
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-06-10 22:24:34 +0900

    up

--- sandbox/iOSWKWebViewSafariVirtualKeyboardDemo/docs/js/index.js
+++ sandbox/iOSWKWebViewSafariVirtualKeyboardDemo/docs/js/index.js
@@ -18,6 +18,8 @@ const createHeader = () => {
 
 const createP = (textContent) => {
   const element = document.createElement('p');
+  element.contentEditable = 'true';
+  //console.log(element)
   element.textContent = textContent;
   return element;
 };
@@ -34,13 +36,13 @@ const mainTag = document.createElement('main');
 const divTag = document.createElement('div');
 divTag.id = 'editor';
 
-const darkred = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-aliquip ex ea commodo consequat. Duis aute irure dolor in
-reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-culpa qui officia deserunt mollit anim id est laborum.`;
+const darkred = ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+ eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+ ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+ aliquip ex ea commodo consequat. Duis aute irure dolor in
+ reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+ pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+ culpa qui officia deserunt mollit anim id est laborum.`;
 
 const darkgoldenrod = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
