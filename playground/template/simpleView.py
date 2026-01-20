0834d5da7045519a70cd1ae016bce87567fe8f90
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-01-21 01:26:25 +0900

    tmp

---
+++ playground/template/simpleView.py
@@ -0,0 +1,103 @@
+top_dir_name = 'pystaRubiconObjcSandBox'
+
+if not __loader__.path[:__loader__.path.rfind('/')].endswith(top_dir_name):
+  import pathlib
+  import sys
+
+  __cwd = pathlib.Path.cwd()
+  __depth_count = str(__cwd).count('/')
+  __root_path = next((__dir for n in range(__depth_count)
+                      if (__dir := __cwd.parents[n]).name == top_dir_name),
+                     None)
+
+  sys.path.append(str(__root_path))
+
+import ctypes
+
+from pyrubicon.objc.api import ObjCClass
+from pyrubicon.objc.api import objc_method, objc_property, objc_const
+from pyrubicon.objc.runtime import send_super, load_library
+from pyrubicon.objc.types import CGRect
+
+from rbedge.functions import NSStringFromClass
+from rbedge import pdbr
+
+UIViewController = ObjCClass('UIViewController')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+
+class MainViewController(UIViewController):
+
+  @objc_method
+  def dealloc(self):
+    # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
+    #print(f'\t - {NSStringFromClass(__class__)}: dealloc')
+    pass
+
+  @objc_method
+  def loadView(self):
+    send_super(__class__, self, 'loadView')
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    self.navigationItem.title = NSStringFromClass(__class__)
+
+  @objc_method
+  def viewWillAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def viewWillDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def didReceiveMemoryWarning(self):
+    send_super(__class__, self, 'didReceiveMemoryWarning')
+    print(f'\t{NSStringFromClass(__class__)}: didReceiveMemoryWarning')
+
+
+if __name__ == '__main__':
+  from rbedge.app import App
+  from rbedge.enumerations import UIModalPresentationStyle
+
+  main_vc = MainViewController.new()
+
+  presentation_style = UIModalPresentationStyle.fullScreen
+  #presentation_style = UIModalPresentationStyle.pageSheet
+
+  app = App(main_vc, presentation_style)
+  app.present()
+
