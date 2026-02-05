a253980229accafd0f66338820576b2af0ac4255
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-02-06 00:43:52 +0900

    override

---
+++ playground/UIView/override/overrideUIView.py
@@ -0,0 +1,168 @@
+_TOP_DIR_NAME = 'pystaRubiconObjcSandBox'
+_MODULES_DIR_NAME = 'modules'
+
+# todo: `./{_TOP_DIR_NAME}/{_MODULES_DIR_NAME}` にあるpackage のimport 準備
+if __name__ == '__main__' and not __file__[:__file__.rfind('/')].endswith(
+    _TOP_DIR_NAME):
+  import pathlib
+  import sys
+  __parents = pathlib.Path(__file__).resolve().parents
+  for path in __parents:
+    if path.name == _TOP_DIR_NAME and (__modules_path :=
+                                       path / _MODULES_DIR_NAME).exists():
+      sys.path.insert(0, str(__modules_path))
+      break
+  else:
+    import warnings
+    with warnings.catch_warnings():
+      warnings.simplefilter('always', ImportWarning)
+      __warning_message = f'./{_TOP_DIR_NAME}/{_MODULES_DIR_NAME} not found in parent directories'
+      warnings.warn(__warning_message, ImportWarning)
+
+import ctypes
+
+from pyrubicon.objc.api import ObjCClass, ObjCInstance
+from pyrubicon.objc.api import objc_method, objc_property
+from pyrubicon.objc.runtime import send_super
+from pyrubicon.objc.types import CGRect
+
+from objc_frameworks.Foundation import NSStringFromClass
+from objc_frameworks.CoreGraphics import CGRectZero
+
+from rbedge import pdbr
+
+UIViewController = ObjCClass('UIViewController')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+UIView = ObjCClass('UIView')
+UIColor = ObjCClass('UIColor')
+
+
+class ColorView(UIView):
+
+  @objc_method
+  def initWithFrame_(self, frame: CGRect) -> ObjCInstance:
+    send_super(__class__, self, 'initWithFrame:', frame, argtypes=[
+      CGRect,
+    ])
+    self.overrideInit()
+
+    return self
+
+  @objc_method
+  def initWithCoder_(self, coder: ctypes.c_void_p) -> ObjCInstance:
+    send_super(__class__,
+               self,
+               'initWithCoder:',
+               coder,
+               argtypes=[
+                 ctypes.c_void_p,
+               ])
+    self.overrideInit()
+    return self
+
+  @objc_method
+  def overrideInit(self):
+    #self.backgroundColor = UIColor.systemRedColor()
+    self.backgroundColor = UIColor.systemPurpleColor()
+
+
+class MainViewController(UIViewController):
+
+  colorView: ColorView = objc_property()
+
+  @objc_method
+  def dealloc(self):
+    # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
+    #print(f'\t - {NSStringFromClass(__class__)}: dealloc')
+    pass
+
+  @objc_method
+  def loadView(self):
+    send_super(__class__, self, 'loadView')
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    self.navigationItem.title = NSStringFromClass(__class__)
+
+    #colorView = ColorView.alloc().initWithFrame_(CGRectZero)
+    colorView = ColorView.new()
+
+    self.view.addSubview_(colorView)
+
+    # --- Layout
+    safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
+
+    colorView.translatesAutoresizingMaskIntoConstraints = False
+    NSLayoutConstraint.activateConstraints_([
+      colorView.centerXAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.centerXAnchor),
+      colorView.centerYAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.centerYAnchor),
+      colorView.widthAnchor.constraintEqualToAnchor_multiplier_(
+        safeAreaLayoutGuide.widthAnchor, 0.5),
+      colorView.heightAnchor.constraintEqualToAnchor_multiplier_(
+        safeAreaLayoutGuide.heightAnchor, 0.5),
+    ])
+
+    self.colorView = colorView
+
+  @objc_method
+  def viewWillAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def viewWillDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def didReceiveMemoryWarning(self):
+    send_super(__class__, self, 'didReceiveMemoryWarning')
+    print(f'\t{NSStringFromClass(__class__)}: didReceiveMemoryWarning')
+
+
+if __name__ == '__main__':
+  from rbedge.app import App
+  from objc_frameworks.UIKit import UIModalPresentationStyle
+
+  main_vc = MainViewController.new()
+
+  presentation_style = UIModalPresentationStyle.fullScreen
+  #presentation_style = UIModalPresentationStyle.pageSheet
+
+  app = App(main_vc, presentation_style)
+  app.present()
+
