02fce42c97087acad6ebdac37e40495c22b90099
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-02-09 16:44:47 +0900

    MTLClearColor

--- modules/objc_frameworks/Metal/MTLRenderPass.py
+++ modules/objc_frameworks/Metal/MTLRenderPass.py
@@ -1,11 +1,11 @@
 import ctypes
-'''
-from pyrubicon.objc.types import with_encoding,with_preferred_encoding
 
-_MTLClearColorEncoding = b'{MTLClearColor=dddd}'
+from pyrubicon.objc.types import with_encoding, with_preferred_encoding
 
 
-@with_preferred_encoding(_MTLClearColorEncoding)
+# ref: `[#L37 | MKCoordinateRegion | toga/iOS/src/toga_iOS/libs/mapkit.py](https://github.com/beeware/toga/blob/f1f9979a939e0d2e27d493c01b4b7b4d5ea19cb1/iOS/src/toga_iOS/libs/mapkit.py#L37)`
+@with_preferred_encoding(b'{MTLClearColor=dddd}')
+@with_encoding(b'{?=dddd}')
 class MTLClearColor(ctypes.Structure):
 
   _fields_ = [
@@ -26,10 +26,4 @@ def MTLClearColorMake(red: ctypes.c_double, green: ctypes.c_double,
                       blue: ctypes.c_double,
                       alpha: ctypes.c_double) -> MTLClearColor:
   return MTLClearColor(red, green, blue, alpha)
-'''
-
-
-def MTLClearColorMake(red: ctypes.c_double, green: ctypes.c_double,
-                      blue: ctypes.c_double, alpha: ctypes.c_double) -> tuple:
-  return (red, green, blue, alpha)
 
