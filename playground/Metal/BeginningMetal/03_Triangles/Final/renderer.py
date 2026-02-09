377ac8cc96a21cfc572ac22d40cbc45d7980ae43
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-02-09 13:05:01 +0900

    vertices

--- playground/Metal/BeginningMetal/03_Triangles/Final/renderer.py
+++ playground/Metal/BeginningMetal/03_Triangles/Final/renderer.py
@@ -19,16 +19,29 @@ if __name__ == '__main__' and not __file__[:__file__.rfind('/')].endswith(
       __warning_message = f'./{_TOP_DIR_NAME}/{_MODULES_DIR_NAME} not found in parent directories'
       warnings.warn(__warning_message, ImportWarning)
 
+import ctypes
+from pathlib import Path
+
 from pyrubicon.objc.api import NSObject
 from pyrubicon.objc.api import objc_method, objc_property
 from pyrubicon.objc.runtime import send_super
 from pyrubicon.objc.types import CGSize
+
+err_ptr = ctypes.c_void_p()
+
+shader_path = Path('./Shader.metal')
+#source = shader_path.read_text('utf-8')
 
 
 class Renderer(NSObject):
 
   device: 'MTLDevice' = objc_property()
   commandQueue: 'MTLCommandQueue' = objc_property()
+  vertices: '[Float]' = (ctypes.c_float * 9)(
+     0.0,  1.0,  0.0,  # 1
+    -1.0, -1.0,  0.0,  # 2
+     1.0, -1.0,  0.0,  # 3
+  )  # yapf: disable
 
   @objc_method
   def initWithDevice_(self, device):
