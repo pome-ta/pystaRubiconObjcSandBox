c3c877d2e1f7d678c2998dfdf586d29c513635e9
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-02-21 00:46:17 +0900

    a-shell run

--- playground/Metal/BeginningMetal/02_GettingStarted/challenge/renderer.py
+++ playground/Metal/BeginningMetal/02_GettingStarted/challenge/renderer.py
@@ -20,15 +20,15 @@ if __name__ == '__main__' and not __file__[:__file__.rfind('/')].endswith(
       warnings.warn(__warning_message, ImportWarning)
 
 from pyrubicon.objc.api import NSObject
-from pyrubicon.objc.api import objc_method
+from pyrubicon.objc.api import objc_method, objc_property
 from pyrubicon.objc.runtime import send_super
 from pyrubicon.objc.types import CGSize
 
 
 class Renderer(NSObject):
 
-  device: 'MTLDevice'
-  commandQueue: 'MTLCommandQueue'
+  device: 'MTLDevice' = objc_property()
+  commandQueue: 'MTLCommandQueue' = objc_property()
 
   @objc_method
   def initWithDevice_(self, device):
