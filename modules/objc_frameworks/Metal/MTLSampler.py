b27e02cbf6071ad1dc56b42b885ac9bd3dd00ef3
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-03-02 11:33:23 +0900

    MTLSampler

--- modules/objc_frameworks/Metal/MTLSampler.py
+++ modules/objc_frameworks/Metal/MTLSampler.py
@@ -1,7 +1,9 @@
 from enum import IntEnum
 
+
 class MTLSamplerMinMagFilter(IntEnum):
   #[doc(alias = "MTLSamplerMinMagFilterNearest")]
   nearest = 0
   #[doc(alias = "MTLSamplerMinMagFilterLinear")]
   linear = 1
+
