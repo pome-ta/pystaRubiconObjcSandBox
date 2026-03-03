7c63a0544b9254af1d64b8852390470a725cd8e9
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-03-03 10:45:14 +0900

    up

--- playground/aShellGround/260302_1854.py
+++ playground/aShellGround/260302_1854.py
@@ -19,16 +19,47 @@ if __name__ == '__main__' and not __file__[:__file__.rfind('/')].endswith(
       __warning_message = f'./{_TOP_DIR_NAME}/{_MODULES_DIR_NAME} not found in parent directories'
       warnings.warn(__warning_message, ImportWarning)
 
-from pyrubicon.objc.api import objc_const, ObjCInstance
+from pathlib import Path
+from pyrubicon.objc.api import ObjCClass, ObjCInstance
+from pyrubicon.objc.api import objc_const
 from pyrubicon.objc.runtime import load_library
 
+from objc_frameworks.Metal import MTLCreateSystemDefaultDevice
 from objc_frameworks.MetalKit import (
   MTKTextureLoaderOptionOrigin,
   MTKTextureLoaderOriginBottomLeft,
 )
 
 
+from rbedge import pdbr
+
+MTKTextureLoader = ObjCClass('MTKTextureLoader')
+
+NSURL = ObjCClass('NSURL')
+
+
+def nsurl(url_or_path):
+  if not isinstance(url_or_path, str):
+    raise TypeError('expected a string')
+  return NSURL.URLWithString_(
+    url_or_path) if ':' in url_or_path else NSURL.fileURLWithPath_(url_or_path)
+
+
+
+device = MTLCreateSystemDefaultDevice()
+textureLoader = MTKTextureLoader.alloc().initWithDevice_(device)
+
 origin = str(MTKTextureLoaderOriginBottomLeft)
+textureLoaderOptions = {str(MTKTextureLoaderOptionOrigin): origin}
+
+image_path = Path('../Metal/BeginningMetal/06_Textures/final/Images/picture.png')#.resolve()
+
+path_str = str(image_path.resolve())
+
+
+texture = textureLoader.newTextureWithContentsOfURL_options_error_(nsurl(path_str), textureLoaderOptions, None)
+
+pdbr.state(texture)
 
 if __name__ == '__main__':
   pass
