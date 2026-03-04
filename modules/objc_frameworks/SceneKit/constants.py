b0ed975eb737728fc7dc64f58891f1b12d8bc23b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-03-04 10:23:35 +0900

    SceneKit

--- modules/objc_frameworks/SceneKit/constants.py
+++ modules/objc_frameworks/SceneKit/constants.py
@@ -1,9 +1,9 @@
 from pyrubicon.objc.api import objc_const, ObjCInstance
 from pyrubicon.objc.runtime import load_library
 
-SceneKit = load_library('SceneKit')
+framework = load_library('SceneKit')
 
 
 def _get_const(global_variable_name) -> ObjCInstance:
-  return objc_const(SceneKit, global_variable_name)
+  return objc_const(framework, global_variable_name)
 
