ed45245b3328da2aa3d0ace5b10f0f04cec15813
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-03-06 00:54:04 +0900

    changed

---
+++ playground/Metal/BeginningMetal/06_Textures/challenge/scenes/gameScene.py
@@ -0,0 +1,37 @@
+from pyrubicon.objc.api import objc_method, objc_property
+from pyrubicon.objc.runtime import send_super, objc_id
+from pyrubicon.objc.types import CGSize
+
+# todo: Pythonista3 の`scene.Scene` ではない
+from .scene import Scene
+from nodes import Plane
+
+
+class GameScene(Scene):
+
+  quad: Plane = objc_property()
+
+  @objc_method
+  def initWithDevice_size_(self, device, size: CGSize):
+
+    self.quad = Plane.alloc().initWithDevice_imageName_maskImageName_(
+      device, 'picture.png', 'picture-frame-mask.png')
+
+    send_super(__class__,
+               self,
+               'initWithDevice:size:',
+               device,
+               size,
+               argtypes=[
+                 objc_id,
+                 CGSize,
+               ])
+
+    self.addChildNode_(self.quad)
+
+    pictureFrame = Plane.alloc().initWithDevice_imageName_(
+      device, 'picture-frame.png')
+    self.addChildNode_(pictureFrame)
+
+    return self
+
