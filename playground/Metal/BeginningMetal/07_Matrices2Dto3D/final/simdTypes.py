ac70a77c9ef03e516827a15e79a96e7f94a84f60
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-03-08 16:55:49 +0900

    up

--- playground/Metal/BeginningMetal/07_Matrices2Dto3D/final/simdTypes.py
+++ playground/Metal/BeginningMetal/07_Matrices2Dto3D/final/simdTypes.py
@@ -1,5 +1,9 @@
 import ctypes
 
+simd_float2 = (ctypes.c_float * 2)
+simd_float3 = (ctypes.c_float * 2)
+simd_float4 = (ctypes.c_float * 4)
+
 Position = (ctypes.c_float * 3)
 Color = (ctypes.c_float * 4)
 Texture = (ctypes.c_float * 2)
