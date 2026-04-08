0191b34938b12f718f7e1a74507dd47e1bae3a1b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-04-08 14:21:04 +0900

    add

---
+++ playground/Metal/BeginningMetal/11_AmbientLighting/challenge02touchesEvent/nodes/__init__.py
@@ -0,0 +1,8 @@
+from .node import Node
+from .plane import Plane
+from .primitive import Primitive
+from .cube import Cube
+from .camera import Camera
+from .model import Model
+from .instance import Instance
+
