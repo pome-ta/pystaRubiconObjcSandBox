cc188605bc485573256af93c2c555c9bc21d9d16
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-04-10 14:24:09 +0900

    up

--- playground/Metal/BeginningMetal/02_GettingStarted/challenge/viewController.py
+++ playground/Metal/BeginningMetal/02_GettingStarted/challenge/viewController.py
@@ -143,9 +143,13 @@ class MainViewController(UIViewController):
       self.metalView.centerYAnchor.constraintEqualToAnchor_(
         safeAreaLayoutGuide.centerYAnchor),
       self.metalView.widthAnchor.constraintEqualToAnchor_multiplier_(
-        safeAreaLayoutGuide.widthAnchor, 0.5),
+        safeAreaLayoutGuide.widthAnchor,
+        0.5,
+      ),
       self.metalView.heightAnchor.constraintEqualToAnchor_multiplier_(
-        safeAreaLayoutGuide.heightAnchor, 0.5),
+        safeAreaLayoutGuide.heightAnchor,
+        0.5,
+      ),
     ])
 
 
