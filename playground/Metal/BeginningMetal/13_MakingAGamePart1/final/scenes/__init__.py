61123fc55fbef8366a1d2b8ef32fc6e61634423c
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-04-10 14:36:57 +0900

    init

---
+++ playground/Metal/BeginningMetal/13_MakingAGamePart1/final/scenes/__init__.py
@@ -0,0 +1,7 @@
+from .scene import Scene
+from .gameScene import GameScene
+from .landscapeScene import LandscapeScene
+from .instanceScene import InstanceScene
+from .crowdScene import CrowdScene
+from .lightingScene import LightingScene
+
