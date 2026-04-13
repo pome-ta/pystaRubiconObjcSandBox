50a79418e6a4324a42eda17470fc780f356cbcfd
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-04-13 17:49:48 +0900

    up

--- playground/Metal/BeginningMetal/13_MakingAGamePart1/challenge/soundController.py
+++ playground/Metal/BeginningMetal/13_MakingAGamePart1/challenge/soundController.py
@@ -1,5 +1,16 @@
-from pyrubicon.objc.api import NSObject
+from pyrubicon.objc.api import ObjCClass, NSObject
 from pyrubicon.objc.api import objc_method, objc_property
+from pyrubicon.objc.runtime import send_super
 
 class SoundController(NSObject):
-  pass
+  
+  
+  
+  backgroundMusicPlayer: 'AVAudioPlayer?' = objc_property()
+  popEffect: 'AVAudioPlayer?' = objc_property()
+  
+  @objc_method
+  def init(self):
+    send_super(__class__, self, 'init')
+
+    return self
