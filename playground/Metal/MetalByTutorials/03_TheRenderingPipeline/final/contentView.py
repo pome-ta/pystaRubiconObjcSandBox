a8ef73c0af54b66d64024efa6210f144257179db
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-05-04 01:20:05 +0900

    contentView.py

--- playground/Metal/MetalByTutorials/03_TheRenderingPipeline/final/contentView.py
+++ playground/Metal/MetalByTutorials/03_TheRenderingPipeline/final/contentView.py
@@ -19,14 +19,11 @@ if __name__ == '__main__' and not __file__[:__file__.rfind('/')].endswith(
       __warning_message = f'./{_TOP_DIR_NAME}/{_MODULES_DIR_NAME} not found in parent directories'
       warnings.warn(__warning_message, ImportWarning)
 
-import ctypes
-
 from pyrubicon.objc.api import ObjCClass
 from pyrubicon.objc.api import objc_method, objc_property
 from pyrubicon.objc.runtime import send_super
-from pyrubicon.objc.types import CGSize, UIEdgeInsetsMake
+from pyrubicon.objc.types import UIEdgeInsetsMake
 
-from objc_frameworks.Foundation import NSStringFromClass
 from objc_frameworks.UIKit import (
   UILayoutConstraintAxis,
   NSTextAlignment,
@@ -35,13 +32,11 @@ from objc_frameworks.UIKit import (
 
 from rbedge import pdbr
 
-UIViewController = ObjCClass('UIViewController')
-
+UIView = ObjCClass('UIView')
 UIStackView = ObjCClass('UIStackView')
 UILabel = ObjCClass('UILabel')
 
 UIColor = ObjCClass('UIColor')
-UIView = ObjCClass('UIView')
 
 
 class ContentView(UIView):
@@ -55,7 +50,7 @@ class ContentView(UIView):
 
   @objc_method
   def setup(self):
-    
+
     metalView = UIView.new()
     metalView.layer.borderWidth = 2.0
     metalView.layer.borderColor = UIColor.separatorColor().CGColor
@@ -83,129 +78,66 @@ class ContentView(UIView):
     self.addSubview_(verticalView)
 
 
-class MainViewController(UIViewController):
-
-  contentView: ContentView = objc_property()
-
-  @objc_method
-  def dealloc(self):
-    # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
-    #print(f'	 - {NSStringFromClass(__class__)}: dealloc')
-    pass
-
-  @objc_method
-  def loadView(self):
-    send_super(__class__, self, 'loadView')
-
-  @objc_method
-  def viewDidLoad(self):
-    send_super(__class__, self, 'viewDidLoad')
-    self.navigationItem.title = NSStringFromClass(__class__)
-    '''
-    metalView = UIView.new()
-    metalView.layer.borderWidth = 2.0
-    metalView.layer.borderColor = UIColor.separatorColor().CGColor
-
-    metalView.backgroundColor = UIColor.systemDarkRedColor()
-
-    text = UILabel.new()
-    text.text = 'Hello, Metal!'
-    text.textAlignment = NSTextAlignment.center
-
-    verticalView = UIStackView.alloc().initWithArrangedSubviews_([
-      metalView,
-      text,
-    ])
-
-    verticalView.axis = UILayoutConstraintAxis.vertical
-    #verticalView.spacing = 16.0
-    verticalView.layoutMargins = UIEdgeInsetsMake(16.0, 16.0, 16.0, 16.0)
-    verticalView.setLayoutMarginsRelativeArrangement_(True)
-
-    verticalView.backgroundColor = UIColor.secondarySystemBackgroundColor()
-    '''
-
-    #self.verticalView = verticalView
-    self.contentView = ContentView.new()
-    #pdbr.state(self.verticalView)
-    self.setupLayoutConstraint()
-
-  @objc_method
-  def viewWillAppear_(self, animated: bool):
-    send_super(__class__,
-               self,
-               'viewWillAppear:',
-               animated,
-               argtypes=[
-                 ctypes.c_bool,
-               ])
-
-  @objc_method
-  def viewDidAppear_(self, animated: bool):
-    send_super(__class__,
-               self,
-               'viewDidAppear:',
-               animated,
-               argtypes=[
-                 ctypes.c_bool,
-               ])
-
-  @objc_method
-  def viewWillDisappear_(self, animated: bool):
-    send_super(__class__,
-               self,
-               'viewWillDisappear:',
-               animated,
-               argtypes=[
-                 ctypes.c_bool,
-               ])
-
-  @objc_method
-  def viewDidDisappear_(self, animated: bool):
-    send_super(__class__,
-               self,
-               'viewDidDisappear:',
-               animated,
-               argtypes=[
-                 ctypes.c_bool,
-               ])
-
-  @objc_method
-  def didReceiveMemoryWarning(self):
-    send_super(__class__, self, 'didReceiveMemoryWarning')
-    print(f'	{NSStringFromClass(__class__)}: didReceiveMemoryWarning')
-
-  # --- private
-  @objc_method
-  def setupLayoutConstraint(self):
-    NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
-
-    self.view.addSubview_(self.contentView)
-
-    safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
-
-    self.contentView.translatesAutoresizingMaskIntoConstraints = False
-    NSLayoutConstraint.activateConstraints_([
-      self.contentView.centerXAnchor.constraintEqualToAnchor_(
-        safeAreaLayoutGuide.centerXAnchor),
-      self.contentView.centerYAnchor.constraintEqualToAnchor_(
-        safeAreaLayoutGuide.centerYAnchor),
-      self.contentView.widthAnchor.constraintEqualToAnchor_multiplier_(
-        safeAreaLayoutGuide.widthAnchor,
-        0.88,
-      ),
-      self.contentView.heightAnchor.constraintEqualToAnchor_multiplier_(
-        safeAreaLayoutGuide.heightAnchor,
-        0.88,
-      ),
-    ])
-
-
 if __name__ == '__main__':
-  from rbedge.app import App
+
+  from objc_frameworks.Foundation import NSStringFromClass
   from objc_frameworks.UIKit import UIModalPresentationStyle
 
-  main_vc = MainViewController.new()
+  from rbedge.app import App
+
+  UIViewController = ObjCClass('UIViewController')
+
+  class TestContentViewController(UIViewController):
+
+    contentView: ContentView = objc_property()
+
+    @objc_method
+    def viewDidLoad(self):
+      send_super(__class__, self, 'viewDidLoad')
+      self.navigationItem.title = NSStringFromClass(__class__)
+
+      self.contentView = ContentView.new()
+
+      self.setupLayoutConstraint()
+
+    @objc_method
+    def didReceiveMemoryWarning(self):
+      send_super(__class__, self, 'didReceiveMemoryWarning')
+      print(f'	{NSStringFromClass(__class__)}: didReceiveMemoryWarning')
+
+    # --- private
+    @objc_method
+    def setupLayoutConstraint(self):
+      NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+      self.view.addSubview_(self.contentView)
+
+      safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
+
+      self.contentView.translatesAutoresizingMaskIntoConstraints = False
+
+      centerXAnchor = self.contentView.centerXAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.centerXAnchor)
+      centerYAnchor = self.contentView.centerYAnchor.constraintEqualToAnchor_(
+        safeAreaLayoutGuide.centerYAnchor)
+
+      widthAnchor = self.contentView.widthAnchor.constraintEqualToAnchor_multiplier_(
+        safeAreaLayoutGuide.widthAnchor,
+        0.88,
+      )
+      heightAnchor = self.contentView.heightAnchor.constraintEqualToAnchor_multiplier_(
+        safeAreaLayoutGuide.heightAnchor,
+        0.88,
+      )
+
+      NSLayoutConstraint.activateConstraints_([
+        centerXAnchor,
+        centerYAnchor,
+        widthAnchor,
+        heightAnchor,
+      ])
+
+  main_vc = TestContentViewController.new()
 
   presentation_style = UIModalPresentationStyle.fullScreen
   #presentation_style = UIModalPresentationStyle.pageSheet
