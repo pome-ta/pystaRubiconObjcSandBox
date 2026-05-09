3cefc2bd556953abf0df73edc0c64b0b2084c244
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-05-09 18:22:37 +0900

    up

--- playground/WKWebView/simpleView.py
+++ playground/WKWebView/simpleView.py
@@ -19,3 +19,140 @@ if __name__ == '__main__' and not __file__[:__file__.rfind('/')].endswith(
       __warning_message = f'./{_TOP_DIR_NAME}/{_MODULES_DIR_NAME} not found in parent directories'
       warnings.warn(__warning_message, ImportWarning)
 
+import ctypes
+from pathlib import Path
+
+from pyrubicon.objc.api import ObjCClass, ObjCInstance
+from pyrubicon.objc.api import objc_method, objc_property
+from pyrubicon.objc.runtime import send_super
+
+from objc_frameworks.Foundation import NSStringFromClass
+from objc_frameworks.UIKit import UIViewAutoresizing
+
+from rbedge import pdbr
+
+UIViewController = ObjCClass('UIViewController')
+WKWebView = ObjCClass('WKWebView')
+
+UIColor = ObjCClass('UIColor')
+
+class MainViewController(UIViewController):
+
+  wkWebView: WKWebView = objc_property()
+
+  @objc_method
+  def dealloc(self):
+    # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
+    #print(f'	 - {NSStringFromClass(__class__)}: dealloc')
+    pass
+
+  @objc_method
+  def loadView(self):
+    send_super(__class__, self, 'loadView')
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    self.navigationItem.title = NSStringFromClass(__class__)
+    
+    self.view.backgroundColor = UIColor.secondarySystemBackgroundColor()
+    
+    wkWebView = WKWebView.new()
+    wkWebView.autoresizingMask = UIViewAutoresizing.flexibleWidth | UIViewAutoresizing.flexibleHeight
+
+    self.wkWebView = wkWebView
+    self.view.addSubview_(self.wkWebView)
+    
+    #self.setupLayoutConstraint()
+
+  @objc_method
+  def viewWillAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    print(self.wkWebView)
+
+  @objc_method
+  def viewWillDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+
+  @objc_method
+  def didReceiveMemoryWarning(self):
+    send_super(__class__, self, 'didReceiveMemoryWarning')
+    print(f'	{NSStringFromClass(__class__)}: didReceiveMemoryWarning')
+
+  # --- private
+  @objc_method
+  def setupLayoutConstraint(self):
+    NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+    self.view.addSubview_(self.wkWebView)
+
+    safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
+
+    self.wkWebView.translatesAutoresizingMaskIntoConstraints = False
+
+    centerXAnchor = self.wkWebView.centerXAnchor.constraintEqualToAnchor_(
+      safeAreaLayoutGuide.centerXAnchor)
+    centerYAnchor = self.wkWebView.centerYAnchor.constraintEqualToAnchor_(
+      safeAreaLayoutGuide.centerYAnchor)
+
+    widthAnchor = self.wkWebView.widthAnchor.constraintEqualToAnchor_multiplier_(
+      safeAreaLayoutGuide.widthAnchor,
+      0.96,
+    )
+    heightAnchor = self.wkWebView.heightAnchor.constraintEqualToAnchor_multiplier_(
+      safeAreaLayoutGuide.heightAnchor,
+      0.96,
+    )
+
+    NSLayoutConstraint.activateConstraints_([
+      centerXAnchor,
+      centerYAnchor,
+      widthAnchor,
+      heightAnchor,
+    ])
+
+
+if __name__ == '__main__':
+  from rbedge.app import App
+  from objc_frameworks.UIKit import UIModalPresentationStyle
+
+  main_vc = MainViewController.new()
+
+  presentation_style = UIModalPresentationStyle.fullScreen
+  #presentation_style = UIModalPresentationStyle.pageSheet
+
+  app = App(main_vc, presentation_style)
+  app.present()
+
