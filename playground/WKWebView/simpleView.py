4af917396bfb3ee9cfac171bff564cddc902f9d0
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-05-12 14:21:23 +0900

    loadFileURL

--- playground/WKWebView/simpleView.py
+++ playground/WKWebView/simpleView.py
@@ -26,7 +26,7 @@ from pyrubicon.objc.api import ObjCClass, ObjCInstance
 from pyrubicon.objc.api import objc_method, objc_property
 from pyrubicon.objc.runtime import send_super
 
-
+from objc_frameworks.CoreGraphics import CGRectZero
 from objc_frameworks.Foundation import NSURLRequestCachePolicy
 from objc_frameworks.UIKit import (
   UIViewAutoresizing,
@@ -39,19 +39,42 @@ from rbedge import pdbr
 
 UIViewController = ObjCClass('UIViewController')
 
-UIView = ObjCClass('UIView')
 WKWebView = ObjCClass('WKWebView')
 WKWebViewConfiguration = ObjCClass('WKWebViewConfiguration')
 WKWebsiteDataStore = ObjCClass('WKWebsiteDataStore')
+NSURL = ObjCClass('NSURL')
 
 UIColor = ObjCClass('UIColor')
 
 
-
-
 class WebViewController(UIViewController):
 
-  wkWebView: WKWebView = objc_property()
+  webView: WKWebView = objc_property()
+  indexPath: Path = objc_property(object)
+
+  @objc_method
+  def initWithIndexPath_(self, indexPath: object):
+    send_super(__class__, self, 'init')
+
+    if not (indexPath.exists()):
+      raise FileNotFoundError(f'{indexPath}')
+
+    self.indexPath = indexPath
+    return self
+
+  @objc_method
+  def makeWeblView(self) -> ObjCInstance:
+    websiteDataStore = WKWebsiteDataStore.nonPersistentDataStore()
+
+    webConfiguration = WKWebViewConfiguration.new()
+    webConfiguration.websiteDataStore = websiteDataStore
+    webConfiguration.preferences.setValue_forKey_(
+      True, 'allowFileAccessFromFileURLs')
+
+    webView = WKWebView.alloc().initWithFrame_configuration_(
+      CGRectZero, webConfiguration)
+
+    return webView
 
   @objc_method
   def dealloc(self):
@@ -62,34 +85,26 @@ class WebViewController(UIViewController):
   @objc_method
   def loadView(self):
     send_super(__class__, self, 'loadView')
-    self.wkWebView = self.makeWeblView()
-
-
-  @objc_method
-  def makeWeblView(self) -> ObjCInstance:
-    webConfiguration = WKWebViewConfiguration.new()
-    websiteDataStore = WKWebsiteDataStore.nonPersistentDataStore()
-    webConfiguration.websiteDataStore = websiteDataStore
-    webConfiguration.preferences.setValue_forKey_(
-      True, 'allowFileAccessFromFileURLs')
-    
-    wkWebView = WKWebView.new()
-    wkWebView.configuration = webConfiguration
-    
-    wkWebView.autoresizingMask = UIViewAutoresizing.flexibleWidth | UIViewAutoresizing.flexibleHeight
-    
-    
-    return wkWebView
-
+    self.webView = self.makeWeblView()
 
   @objc_method
   def viewDidLoad(self):
     from objc_frameworks.Foundation import NSStringFromClass
-    
+
     send_super(__class__, self, 'viewDidLoad')
     self.navigationItem.title = NSStringFromClass(__class__)
 
-    
+    # --- load
+    _fileURLWithPath = NSURL.fileURLWithPath_isDirectory_
+    _path = str(self.indexPath)
+    _parent = str(self.indexPath.parent)
+    loadFileURL = _fileURLWithPath(_path, False)
+    allowingReadAccessToURL = _fileURLWithPath(_parent, True)
+
+    self.webView.loadFileURL(
+      loadFileURL,
+      allowingReadAccessToURL=allowingReadAccessToURL,
+    )
 
     self.setupLayoutConstraint()
 
@@ -103,7 +118,6 @@ class WebViewController(UIViewController):
                  ctypes.c_bool,
                ])
 
-
   @objc_method
   def viewDidAppear_(self, animated: bool):
     send_super(__class__,
@@ -114,7 +128,6 @@ class WebViewController(UIViewController):
                  ctypes.c_bool,
                ])
 
-
   @objc_method
   def viewWillDisappear_(self, animated: bool):
     send_super(__class__,
@@ -124,6 +137,7 @@ class WebViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
+    self.webView.reloadFromOrigin()
 
   @objc_method
   def viewDidDisappear_(self, animated: bool):
@@ -145,22 +159,22 @@ class WebViewController(UIViewController):
   def setupLayoutConstraint(self):
     NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
 
-    self.view.addSubview_(self.wkWebView)
+    self.view.addSubview_(self.webView)
 
     safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
 
-    self.wkWebView.translatesAutoresizingMaskIntoConstraints = False
+    self.webView.translatesAutoresizingMaskIntoConstraints = False
 
-    centerXAnchor = self.wkWebView.centerXAnchor.constraintEqualToAnchor_(
+    centerXAnchor = self.webView.centerXAnchor.constraintEqualToAnchor_(
       safeAreaLayoutGuide.centerXAnchor)
-    centerYAnchor = self.wkWebView.centerYAnchor.constraintEqualToAnchor_(
+    centerYAnchor = self.webView.centerYAnchor.constraintEqualToAnchor_(
       safeAreaLayoutGuide.centerYAnchor)
 
-    widthAnchor = self.wkWebView.widthAnchor.constraintEqualToAnchor_multiplier_(
+    widthAnchor = self.webView.widthAnchor.constraintEqualToAnchor_multiplier_(
       safeAreaLayoutGuide.widthAnchor,
       0.96,
     )
-    heightAnchor = self.wkWebView.heightAnchor.constraintEqualToAnchor_multiplier_(
+    heightAnchor = self.webView.heightAnchor.constraintEqualToAnchor_multiplier_(
       safeAreaLayoutGuide.heightAnchor,
       0.96,
     )
@@ -177,7 +191,10 @@ if __name__ == '__main__':
   from rbedge.app import App
   from objc_frameworks.UIKit import UIModalPresentationStyle
 
-  main_vc = WebViewController.new()
+  index_path = Path('./docs/index.html')
+
+  #main_vc = WebViewController.new()
+  main_vc = WebViewController.alloc().initWithIndexPath_(index_path)
 
   presentation_style = UIModalPresentationStyle.fullScreen
   #presentation_style = UIModalPresentationStyle.pageSheet
