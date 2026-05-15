88b23a05cba7f4ba2d28fea8f0ef3caa3a528806
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-05-15 10:22:56 +0900

    tool closes

--- playground/UIView/LiquidGlassNavigation/toolbar.py
+++ playground/UIView/LiquidGlassNavigation/toolbar.py
@@ -21,16 +21,155 @@ if __name__ == '__main__' and not __file__[:__file__.rfind('/')].endswith(
 
 import ctypes
 
-from pyrubicon.objc.api import ObjCClass
+from pyrubicon.objc.api import ObjCClass, ObjCProtocol
 from pyrubicon.objc.api import objc_method, objc_property
-from pyrubicon.objc.runtime import send_super
+from pyrubicon.objc.runtime import send_super, SEL
 
 from objc_frameworks.UIKit import UIViewAutoresizing
+from objc_frameworks.UIKit import UIBarButtonSystemItem
 
+from rbedge.lifeCycle import loop
 from rbedge import pdbr
+
+UINavigationControllerDelegate = ObjCProtocol('UINavigationControllerDelegate')
+
+UINavigationController = ObjCClass('UINavigationController')
+UINavigationBarAppearance = ObjCClass('UINavigationBarAppearance')
+UIToolbarAppearance = ObjCClass('UIToolbarAppearance')
+UIViewController = ObjCClass('UIViewController')
 
 UIView = ObjCClass('UIView')
 UIColor = ObjCClass('UIColor')
+UIBarButtonItem = ObjCClass('UIBarButtonItem')
+
+
+class NavigationController(
+    UINavigationController,
+    protocols=[
+      UINavigationControllerDelegate,
+    ],
+):
+
+  @objc_method
+  def dealloc(self):
+    # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
+    #print(f'- {NSStringFromClass(__class__)}: dealloc')
+    loop.stop()
+
+  @objc_method
+  def loadView(self):
+    send_super(__class__, self, 'loadView')
+    #print(f'{NSStringFromClass(__class__)}: loadView')
+    
+    navigationBarAppearance = UINavigationBarAppearance.new()
+    navigationBarAppearance.configureWithDefaultBackground()
+
+    self.navigationBar.standardAppearance = navigationBarAppearance
+    self.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+    self.navigationBar.compactAppearance = navigationBarAppearance
+    self.navigationBar.compactScrollEdgeAppearance = navigationBarAppearance
+    
+    
+    '''
+    toolbarAppearance = UIToolbarAppearance.new()
+    toolbarAppearance.configureWithDefaultBackground()
+
+    self.toolbar.standardAppearance = toolbarAppearance
+    self.toolbar.scrollEdgeAppearance = toolbarAppearance
+    self.toolbar.compactAppearance = toolbarAppearance
+    self.toolbar.compactScrollEdgeAppearance = toolbarAppearance
+    '''
+    
+    #
+    #self.setToolbarHidden_animated_(False, True)
+    #self.setNavigationBarHidden_animated_(True, True)
+    #self.setToolbarHidden_(False)
+    #pdbr.state(self)
+    #print(self.toolbarHidden)
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    #print(f'{NSStringFromClass(__class__)}: viewDidLoad')
+    self.delegate = self
+
+  @objc_method
+  def viewWillAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print(f'{NSStringFromClass(__class__)}: viewWillAppear_')
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print(f'{NSStringFromClass(__class__)}: viewDidAppear_')
+
+  @objc_method
+  def viewWillDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print(f'{NSStringFromClass(__class__)}: viewWillDisappear_')
+    #self.setToolbarHidden_(True)
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print(f'{NSStringFromClass(__class__)}: viewDidDisappear_')
+
+  @objc_method
+  def didReceiveMemoryWarning(self):
+    send_super(__class__, self, 'didReceiveMemoryWarning')
+    print(f'{NSStringFromClass(__class__)}: didReceiveMemoryWarning')
+
+  @objc_method
+  def navigationController_willShowViewController_animated_(
+      self, navigationController, viewController, animated: bool):
+    # xxx: layout 範囲の制限
+    #extendedLayout = UIRectEdge.none
+    #viewController.setEdgesForExtendedLayout_(extendedLayout)
+
+    closeButtonItem = UIBarButtonItem.alloc().initWithBarButtonSystemItem(
+      UIBarButtonSystemItem.close,
+      target=navigationController,
+      action=SEL('doneButtonTapped:'),
+    )
+
+    # todo: view 遷移でのButton 重複を判別
+
+    visibleViewController = navigationController.visibleViewController
+
+    #pdbr.state(visibleViewController)
+
+    navigationItem = visibleViewController.navigationItem
+    navigationItem.leftBarButtonItem = closeButtonItem
+
+    #navigationController.setToolbarItems_animated_([closeButtonItem] ,True)
+
+  @objc_method
+  def doneButtonTapped_(self, sender):
+    self.dismissViewControllerAnimated_completion_(True, None)
 
 
 class MainViewController(ObjCClass('UIViewController')):
@@ -51,7 +190,8 @@ class MainViewController(ObjCClass('UIViewController')):
   def viewDidLoad(self):
     from objc_frameworks.Foundation import NSStringFromClass
     send_super(__class__, self, 'viewDidLoad')
-    self.navigationItem.title = NSStringFromClass(__class__)
+    #self.navigationItem.title = NSStringFromClass(__class__)
+    self.view.backgroundColor = UIColor.systemDarkPinkColor()
 
     subView = UIView.new()
     subView.autoresizingMask = UIViewAutoresizing.flexibleWidth | UIViewAutoresizing.flexibleHeight
@@ -59,8 +199,22 @@ class MainViewController(ObjCClass('UIViewController')):
     subView.backgroundColor = UIColor.systemDarkTealColor()
 
     self.subView = subView
+    #pdbr.state(self)
+    closeButtonItem = UIBarButtonItem.alloc().initWithBarButtonSystemItem(
+      UIBarButtonSystemItem.close,
+      target=self.navigationController,
+      action=SEL('doneButtonTapped:'),
+    )
+    self.toolbarItems = [closeButtonItem]
 
     self.setupLayoutConstraint()
+
+  @objc_method
+  def doneButtonTapped_(self, sender):
+    self.navigationController.dismissViewControllerAnimated(
+      True,
+      completion=None,
+    )
 
   @objc_method
   def viewWillAppear_(self, animated: bool):
@@ -71,6 +225,7 @@ class MainViewController(ObjCClass('UIViewController')):
                argtypes=[
                  ctypes.c_bool,
                ])
+    self.navigationController.setToolbarHidden_animated_(False, animated)
 
   @objc_method
   def viewDidAppear_(self, animated: bool):
@@ -151,5 +306,5 @@ if __name__ == '__main__':
   #presentation_style = UIModalPresentationStyle.pageSheet
 
   app = App(main_vc, presentation_style)
-  app.present()
+  app.present(NavigationController)
 
